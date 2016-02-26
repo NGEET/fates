33e34c622260e6a9017aea685c9536a619eb6a56
Charlie Koven <cdkoven@lbl.gov>
2016-02-26 15:42:40 -0800

    added first instance of NEP and NBP fluxes

--- components/clm/src/main/clm_driver.F90
+++ components/clm/src/main/clm_driver.F90
@@ -826,7 +826,8 @@ contains
                soilbiogeochem_carbonflux_inst, soilbiogeochem_carbonstate_inst,         &
                c13_soilbiogeochem_carbonflux_inst, c13_soilbiogeochem_carbonstate_inst, &
                c14_soilbiogeochem_carbonflux_inst, c14_soilbiogeochem_carbonstate_inst, &
-               soilbiogeochem_nitrogenflux_inst, soilbiogeochem_nitrogenstate_inst)
+               soilbiogeochem_nitrogenflux_inst, soilbiogeochem_nitrogenstate_inst,     &
+               ed_clm_inst, ed_allsites_inst(bounds_clump%begg:bounds_clump%endg)
        end if
 
        ! ============================================================================
