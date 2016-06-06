30862a970f7e9462e1c206f59750fdfbb2a0efa6
Ryan Knox <rgknox@gmail.com>
2016-06-06 11:18:39 -0700

    added a check to see if any unallocated site vectors exist

--- main/EDCLMLinkMod.F90
+++ main/EDCLMLinkMod.F90
@@ -1194,9 +1194,8 @@ contains
       end do ! column loop
 
       call this%flux_into_litter_pools(bounds, sites(:), nsites, fcolumn(:), canopystate_inst)
-      
       call this%ed_update_history_variables(bounds, sites(:), nsites, fcolumn(:), canopystate_inst)
-
+      
     end associate
 
   end subroutine ed_clm_link
