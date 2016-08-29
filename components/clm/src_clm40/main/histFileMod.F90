47ea4f446230c8ce8e97d1f7545253505056ed13
Ben Andre <andre@ucar.edu>
2016-08-29 14:57:06 -0600

    'pull clm4_5_12_r192 tags from svn'

--- components/clm/src_clm40/main/histFileMod.F90
+++ components/clm/src_clm40/main/histFileMod.F90
@@ -1770,7 +1770,7 @@ contains
     call ncd_putatt(lnfid, ncd_global, 'version' , trim(version))
 
     str = &
-    '$Id: histFileMod.F90 80157 2016-08-21 12:20:26Z sacks $'
+    '$Id: histFileMod.F90 80242 2016-08-27 21:28:13Z andre@ucar.edu $'
     call ncd_putatt(lnfid, ncd_global, 'revision_id', trim(str))
     call ncd_putatt(lnfid, ncd_global, 'case_title', trim(ctitle))
     call ncd_putatt(lnfid, ncd_global, 'case_id', trim(caseid))
