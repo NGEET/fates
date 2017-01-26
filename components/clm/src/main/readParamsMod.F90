d4c80ef55e6d53caabbbbcb1547120ae7d1843e2
Ben Andre <andre@ucar.edu>
2017-01-26 10:00:25 -0700

    New infrastructure to read fates array parameters.
    Register and receive array parameters in fates with the host
    reading. Host allocates the data buffer based on the size of the
    largest used parameter dimension. Tested with spitfire array
    parameters. Some error checking of dimension sizes read from file vs
    memory size that fates is expecting.
    
    User interface changes?: no
    
    Test suite: ed - yellowstone gnu, intel, pgi
    Test baseline: a651a4f
    Test namelist changes: fates_paramfile
    Test answer changes: bit for bit
    Test summary: all tests pass
    
    Test suite: clm_short - yellowstone gnu, intel, pgi
    Test baseline: clm4_5_12_r195
    Test namelist changes: none
    Test answer changes: bit for bit
    Test summary: all tests pass

--- components/clm/src/main/readParamsMod.F90
+++ components/clm/src/main/readParamsMod.F90
@@ -117,11 +117,13 @@ contains
     use clm_varctl, only : fates_paramfile
     use shr_kind_mod, only: r8 => shr_kind_r8
     use paramUtilMod, only : readNcdio
+    use abortutils, only : endrun
 
     use EDParamsMod, only : FatesRegisterParams, FatesReceiveParams
-    use SFParamsMod, only : SFParamsRead, SpitFireRegisterParams, SpitFireReceiveParams
+    use SFParamsMod, only : SpitFireRegisterParams, SpitFireReceiveParams
+
     use FatesParametersInterface, only : fates_parameters_type
-    use FatesParametersInterface, only : param_string_length
+    use FatesParametersInterface, only : param_string_length, max_dimensions, max_used_dimensions
     use FatesParametersInterface, only : dimension_shape_scalar, dimension_shape_1d, dimension_shape_2d
     use FatesParametersInterface, only : dimension_name_pft
 
@@ -130,12 +132,14 @@ contains
     character(len=256) :: locfn ! local file name
     type(file_desc_t)  :: ncid  ! pio netCDF file id
     integer            :: dimid ! netCDF dimension id
-    integer            :: npft  ! number of pfts on pft-physiology file
     character(len=32)  :: subname = 'readFatesParameters'
     class(fates_parameters_type), allocatable :: fates_params
     integer :: i, num_params, dimension_shape
+    integer :: max_dim_size
     real(r8), allocatable :: data(:, :)
     character(len=param_string_length) :: name
+    integer :: dimension_sizes(max_dimensions)
+    integer :: size_dim_1, size_dim_2
 
     if (use_ed) then
        if (masterproc) then
@@ -144,42 +148,97 @@ contains
 
        call getfil (fates_paramfile, locfn, 0)
        call ncd_pio_openfile (ncid, trim(locfn), 0)
-       call ncd_inqdid(ncid, dimension_name_pft, dimid)
-       call ncd_inqdlen(ncid, dimid, npft)
-
-       ! read using the old infrastrructure
-       call SFParamsRead(ncid)
 
        ! read parameters with new fates parammeter infrastructure
        allocate(fates_params)
-       allocate(data(npft, npft)) ! FIXME(bja, 2017-01) correct? maxpft? needs to be max of all dimensions expected to receive!
        call fates_params%Init()
        call FatesRegisterParams(fates_params)
        call SpitFireRegisterParams(fates_params)
+
+       call set_parameter_dimensions(ncid, fates_params)
+       max_dim_size = fates_params%GetMaxDimensionSize()
+       allocate(data(max_dim_size, max_dim_size))
+
        num_params = fates_params%num_params()
        do i = 1, num_params
-          call fates_params%GetMetaData(i, name, dimension_shape)
+          call fates_params%GetMetaData(i, name, dimension_shape, dimension_sizes)
           select case(dimension_shape)
           case(dimension_shape_scalar)
-             call readNcdio(ncid, name, subname, data(1, 1))
-             call fates_params%SetData(i, data(1, 1))
+             size_dim_1 = 1
+             size_dim_2 = 1
           case(dimension_shape_1d)
-             call readNcdio(ncid, name, subname, data(:, 1))
-             call fates_params%SetData(i, data(:, 1))
+             size_dim_1 = dimension_sizes(1)
+             size_dim_2 = 1
           case(dimension_shape_2d)
-             call readNcdio(ncid, name, subname, data(:, :))
-             call fates_params%SetData(i, data(:, :))
+             size_dim_1 = dimension_sizes(1)
+             size_dim_2 = dimension_sizes(2)
           case default
-             ! error, unsupported number of dimensions
+             call endrun(msg='unsupported number of dimensions reading parameters.')
           end select
+          call readNcdio(ncid, name, subname, data(1:size_dim_1, 1:size_dim_2))
+          call fates_params%SetData(i, data(1:size_dim_1, 1:size_dim_2))
        end do
        call FatesReceiveParams(fates_params)
        call SpitFireReceiveParams(fates_params)
        deallocate(data)
+
+       call fates_params%Destroy()
        deallocate(fates_params)
        call ncd_pio_closefile(ncid)
     end if
 
   end subroutine readFatesParameters
+
+  !-----------------------------------------------------------------------
+  subroutine set_parameter_dimensions(ncid, fates_params)
+    ! Get the list of dimensions used by the fates parameters,
+    ! retreive them from the parameter file, then give the information
+    ! back to fates.
+    use FatesParametersInterface, only : fates_parameters_type, param_string_length, max_dimensions, max_used_dimensions
+
+    implicit none
+
+    type(file_desc_t), intent(inout) :: ncid
+    class(fates_parameters_type), intent(inout) :: fates_params
+
+    integer :: num_used_dimensions
+    character(len=param_string_length) :: used_dimension_names(max_used_dimensions)
+    integer :: used_dimension_sizes(max_used_dimensions)
+    
+    call fates_params%GetUsedDimensions(num_used_dimensions, used_dimension_names)
+
+    call get_used_dimension_sizes(ncid, num_used_dimensions, used_dimension_names, used_dimension_sizes)
+
+    call fates_params%SetDimensionSizes(num_used_dimensions, used_dimension_names, used_dimension_sizes)
+    
+  end subroutine set_parameter_dimensions
+     
+  !-----------------------------------------------------------------------
+  subroutine get_used_dimension_sizes(ncid, num_used_dimensions, dimension_names, dimension_sizes)
+
+    use FatesParametersInterface, only : param_string_length
+    
+    implicit none
+
+    type(file_desc_t), intent(inout) :: ncid
+    integer, intent(in) :: num_used_dimensions
+    character(len=param_string_length), intent(in) :: dimension_names(:)
+    integer, intent(out) :: dimension_sizes(:)
+
+    integer :: d, max_dim_size, num_dims
+    integer :: dim_len, dim_id
+
+
+    dimension_sizes(:) = 0
+    max_dim_size = 0
+
+    do d = 1, num_used_dimensions
+       call ncd_inqdid(ncid, dimension_names(d), dim_id)
+       call ncd_inqdlen(ncid, dim_id, dim_len)
+       dimension_sizes(d) = dim_len
+       write(*, *) '--> ', trim(dimension_names(d)), ' setting size ', dimension_sizes(d)
+    end do
+    
+  end subroutine get_used_dimension_sizes
 
 end module readParamsMod
