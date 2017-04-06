917420c9ed03437d43c842d941b223c83a0def65
ckoven <ckoven@gmail.com>
2017-04-06 15:42:34 -0700

    bugfix for restarting

--- components/clm/src/ED/main/FatesRestartInterfaceMod.F90
+++ components/clm/src/ED/main/FatesRestartInterfaceMod.F90
@@ -1423,7 +1423,8 @@ contains
                 if ((mod(fto, 2)  ==  0 )) then
                    ft=1
                 endif
-                
+                temp_cohort%pft = ft
+
                 cohortstatus = newp%siteptr%status
 
                 if(EDPftvarcon_inst%stress_decid(ft) == 1)then !drought decidous, override status. 
