64ce2c486e77d98270099bbaeaa3e051e3f12fc5
Ben Andre <bjandre@gmail.com>
2017-04-06 14:22:56 -0600

    'pull clm4_5_15_r232 tags from svn'

--- components/clm/src_clm40/main/histFileMod.F90
+++ components/clm/src_clm40/main/histFileMod.F90
@@ -1770,7 +1770,7 @@ contains
     call ncd_putatt(lnfid, ncd_global, 'version' , trim(version))
 
     str = &
-    '$Id: histFileMod.F90 84186 2017-04-03 12:45:05Z sacks $'
+    '$Id: histFileMod.F90 84241 2017-04-05 22:06:48Z erik $'
     call ncd_putatt(lnfid, ncd_global, 'revision_id', trim(str))
     call ncd_putatt(lnfid, ncd_global, 'case_title', trim(ctitle))
     call ncd_putatt(lnfid, ncd_global, 'case_id', trim(caseid))
