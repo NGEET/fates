ecd94dd17bdb8015218995d68256222f590ae3d9
Ryan Knox <rgknox@gmail.com>
2018-04-02 14:11:40 -0400

    Add initialization of ifire disturbance rate

--- biogeochem/EDPatchDynamicsMod.F90
+++ biogeochem/EDPatchDynamicsMod.F90
@@ -151,7 +151,8 @@ contains
        
        currentPatch%disturbance_rates(dtype_ifall) = 0.0_r8
        currentPatch%disturbance_rates(dtype_ilog)  = 0.0_r8
-
+       currentPatch%disturbance_rates(dtype_ifire) = 0.0_r8
+       
        currentCohort => currentPatch%shortest
        do while(associated(currentCohort))   
 
