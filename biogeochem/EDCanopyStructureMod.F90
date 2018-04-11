892f5d4358ffa55c5983cc4a29471077e6c50dd6
Erik Kluzek <erik@ucar.edu>
2018-04-11 15:31:39 -0600

    Shorten lines so can compile with nag

--- biogeochem/EDCanopyStructureMod.F90
+++ biogeochem/EDCanopyStructureMod.F90
@@ -447,9 +447,10 @@ contains
                         currentCohort%n = 0.0_r8
                         currentCohort%c_area = 0._r8
                      else  
-                        call carea_allom(currentCohort%dbh,currentCohort%n,currentSite%spread,currentCohort%pft,currentCohort%c_area)
+                        call carea_allom(currentCohort%dbh,currentCohort%n,currentSite%spread,currentCohort%pft, &
+                                         currentCohort%c_area)
                      endif
-                     
+
                      call carea_allom(copyc%dbh,copyc%n,currentSite%spread,copyc%pft,copyc%c_area)
 
 
@@ -522,7 +523,8 @@ contains
                         currentCohort%c_area = 0._r8
 
                      else  
-                        call carea_allom(currentCohort%dbh,currentCohort%n,currentSite%spread,currentCohort%pft,currentCohort%c_area)
+                        call carea_allom(currentCohort%dbh,currentCohort%n,currentSite%spread, &
+                                         currentCohort%pft,currentCohort%c_area)
                      endif
 
                   endif ! matches: if (cc_loss < currentCohort%c_area)then
