040a2c48ff4d70e8894a05acf14ed7be8bfc485f
Jacquelyn Shuman <jkshuman@ucar.edu>
2018-10-03 14:36:42 -0600

    Correct typo for diffuse surface albedo
    Correct typo to set the diffuse surface albedo correctly as identified by JHolm
    
    Fixes:408
    
    User interface changes?: No
    
    Code review: JKShuman, JHolm, RKnox, RFisher
    
    Test suite: Hobart and Cheyenne
    Test baseline:
    Test namelist changes: none
    Test answer changes:expect changes
    Test summary: will run on Hobart and follow with baseline tests on Cheyenne

--- biogeophys/EDSurfaceAlbedoMod.F90
+++ biogeophys/EDSurfaceAlbedoMod.F90
@@ -196,7 +196,7 @@ contains
                     bc_out(s)%fabi_parb(ifp,:) = 0.0_r8
                     do ib = 1,hlm_numSWb
                        bc_out(s)%albd_parb(ifp,ib) = bc_in(s)%albgr_dir_rb(ib)
-                       bc_out(s)%albd_parb(ifp,ib) = bc_in(s)%albgr_dif_rb(ib)
+                       bc_out(s)%albi_parb(ifp,ib) = bc_in(s)%albgr_dif_rb(ib)
                        bc_out(s)%ftdd_parb(ifp,ib)= 1.0_r8
                        bc_out(s)%ftid_parb(ifp,ib)= 1.0_r8
                        bc_out(s)%ftii_parb(ifp,ib)= 1.0_r8
