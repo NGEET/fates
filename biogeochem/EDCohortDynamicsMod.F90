6abcbe6c32f0a8af3edb48d30e401e2e38657105
Chonggang Xu <xuchongang@gmail.com>
2018-11-15 10:58:34 -0700

    update the leaf lai for hydro after fuse cohorts

--- biogeochem/EDCohortDynamicsMod.F90
+++ biogeochem/EDCohortDynamicsMod.F90
@@ -733,6 +733,7 @@ contains
      real(r8) :: newn
      real(r8) :: diff
      real(r8) :: dynamic_fusion_tolerance
+     real(r8) :: leaf_c             ! leaf carbon [kg]
 
      integer  :: largersc, smallersc, sc_i        ! indices for tracking the growth flux caused by fusion
      real(r8) :: larger_n, smaller_n
@@ -851,11 +852,7 @@ contains
                                       currentCohort%size_class,currentCohort%size_by_pft_class)
 				      
 
-                                if(hlm_use_planthydro.eq.itrue) then
-				    call carea_allom(currentCohort%dbh,currentCohort%n,currentSite%spread, &
-				          currentCohort%pft,currentCohort%c_area)
-                                    currentCohort%treelai = tree_lai(currentCohort%bl, currentCohort%status_coh, &
-                                          currentCohort%pft, currentCohort%c_area, currentCohort%n )					  
+                                if(hlm_use_planthydro.eq.itrue) then			  					  				  
 				    call FuseCohortHydraulics(currentSite,currentCohort,nextc,bc_in,newn)				    
 				 endif
 
@@ -987,7 +984,18 @@ contains
                                 endif
                                 
                                 ! At this point, nothing should be pointing to current Cohort
-                                if (hlm_use_planthydro.eq.itrue) call DeallocateHydrCohort(nextc)
+				! update hydraulics quantities that are functions of hite & biomasses
+				! deallocate the hydro structure of nextc
+                                if (hlm_use_planthydro.eq.itrue) then				    
+				    call carea_allom(currentCohort%dbh,currentCohort%n,currentSite%spread, &
+				          currentCohort%pft,currentCohort%c_area)
+                                    leaf_c   = currentCohort%prt%GetState(leaf_organ, all_carbon_elements)
+                                    currentCohort%treelai = tree_lai(leaf_c,             &
+                                       currentCohort%pft, currentCohort%c_area, currentCohort%n, &
+                                       currentCohort%canopy_layer, currentPatch%canopy_layer_tlai )			    
+				   call updateSizeDepTreeHydProps(currentSite,currentCohort, bc_in)  				   
+				   call DeallocateHydrCohort(nextc)
+				endif
 
                                 ! Deallocate the cohort's PRT structure
                                 call nextc%prt%DeallocatePRTVartypes()
