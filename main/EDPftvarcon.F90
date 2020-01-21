e45153c7271edcef6c4edce82976c7a80e5eb040
JessicaNeedham <jfneedham@lbl.gov>
2020-01-21 13:48:45 -0800

    [ More detailed flags for mortality parameter checks in EDPftvarcon ]
    [Check all size and age mortality parameters are positive and
    turned on in the correct combinations. ]
    
    Fixes: [NGT-ED Github issue #]
    
    User interface changes?: [Yes (describe what changes), No]
    
    Code review: [Names]
    
    Test suite: [suite name, machine, compilers]
    Test baseline:
    Test namelist changes:
    Test answer changes: [bit for bit, roundoff, climate changing]
    Test summary: Appears to work as expected on lobata.

--- main/EDPftvarcon.F90
+++ main/EDPftvarcon.F90
@@ -2189,11 +2189,13 @@ contains
 
         ! Check that parameter ranges for age-dependent mortality make sense   
         !-----------------------------------------------------------------------------------    
-        if ( ( EDPftvarcon_inst%mort_ip_age_senescence(ipft) < 0.0_r8 ) ) then
+        if ( ( EDPftvarcon_inst%mort_ip_age_senescence(ipft) < 0.0_r8 ) .or. &
+           (EDPftvarcon_inst%mort_r_age_senescence(ipft) < 0.0_r8 ) ) then
 
-           write(fates_log(),*) 'Negative mort_ip_age_senescence means'
-           write(fates_log(),*) 'trees will die at a negative age.'
-           write(fates_log(),*) 'Sensible values are between 10 and 2000?'
+           write(fates_log(),*) 'Either mort_ip_age_senescence or mort_r_age_senescece'
+           write(fates_log(),*) 'is negative which makes no biological sense.'
+           write(fates_log(),*) 'Sensible values for ip are between 10 and 2000?'
+           write(fates_log(),*) 'Sensible values for r are between 0.03-0.06'
            write(fates_log(),*) 'Aborting'
            call endrun(msg=errMsg(sourcefile, __LINE__))
         end if
@@ -2212,11 +2214,13 @@ contains
 
         ! Check that parameter ranges for size-dependent mortality make sense   
         !-----------------------------------------------------------------------------------    
-        if ( ( EDPftvarcon_inst%mort_ip_size_senescence(ipft) < 0.0_r8 ) ) then
+        if ( ( EDPftvarcon_inst%mort_ip_size_senescence(ipft) < 0.0_r8 ) .or. &
+           ( EDPftvarcon_inst%mort_r_size_senescence(ipft) < 0.0_r8 ) ) then
 
-           write(fates_log(),*) 'Negative mort_ip_size_senescence means'
-           write(fates_log(),*) 'trees will die at a negative dbh.'
-           write(fates_log(),*) 'Sensible values are between 1 and 300?'
+           write(fates_log(),*) 'Either mort_ip_size_senescence or mort_r_size_senescence'
+           write(fates_log(),*) 'is negative which makes no biological sense.'
+           write(fates_log(),*) 'Sensible values for ip are between 1 and 300?'
+           write(fates_log(),*) 'Sensible values for r are between 0.03-0.06'
            write(fates_log(),*) 'Aborting'
            call endrun(msg=errMsg(sourcefile, __LINE__))
         end if
