32620d77d874357c7302253685d8e48206b4245a
ckoven <ckoven@gmail.com>
2021-01-06 21:47:43 -0700

    zeroing patch fire vars for first timestep

--- main/EDInitMod.F90
+++ main/EDInitMod.F90
@@ -357,6 +357,7 @@ contains
      
      type(ed_site_type),  pointer :: sitep
      type(ed_patch_type), pointer :: newp
+     type(ed_patch_type), pointer :: currentPatch
 
      ! List out some nominal patch values that are used for Near Bear Ground initializations
      ! as well as initializing inventory
@@ -437,6 +438,35 @@ contains
 
      end if
 
+     ! zero all the patch fire variables for the first timestep
+     do s = 1, nsites
+        currentPatch => sites(s)%youngest_patch
+        do while(associated(currentPatch))
+
+           currentPatch%litter_moisture(:)         = 0._r8
+           currentPatch%fuel_eff_moist             = 0._r8
+           currentPatch%livegrass                  = 0._r8
+           currentPatch%sum_fuel                   = 0._r8
+           currentPatch%fuel_bulkd                 = 0._r8
+           currentPatch%fuel_sav                   = 0._r8
+           currentPatch%fuel_mef                   = 0._r8
+           currentPatch%ros_front                  = 0._r8
+           currentPatch%effect_wspeed              = 0._r8
+           currentPatch%tau_l                      = 0._r8
+           currentPatch%fuel_frac(:)               = 0._r8
+           currentPatch%tfc_ros                    = 0._r8
+           currentPatch%fi                         = 0._r8
+           currentPatch%fire                       = 0
+           currentPatch%fd                         = 0._r8
+           currentPatch%ros_back                   = 0._r8
+           currentPatch%scorch_ht(:)               = 0._r8
+           currentPatch%frac_burnt                 = 0._r8
+           currentPatch%burnt_frac_litter(:)       = 0._r8
+
+           currentPatch => currentPatch%older
+        enddo
+     enddo
+     
      ! This sets the rhizosphere shells based on the plant initialization
      ! The initialization of the plant-relevant hydraulics variables
      ! were set from a call inside of the init_cohorts()->create_cohort() subroutine
