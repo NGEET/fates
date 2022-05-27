64f6c719f94f28d9ab9138363dde2ee4f7124600
Ryan Knox <rgknox@lbl.gov>
2022-05-27 13:19:45 -0400

    Removing export frac check

--- main/EDPftvarcon.F90
+++ main/EDPftvarcon.F90
@@ -1577,14 +1577,6 @@ contains
         call endrun(msg=errMsg(sourcefile, __LINE__))
      endif
 
-     ! logging export turned off in CLM until the interface is included
-     if(logging_export_frac>nearzero .and. hlm_name .eq. 'CLM') then
-        write(fates_log(),*) 'The harvest to wood-product interface with CLM is not yet operational'
-        write(fates_log(),*) 'This feature is only available in ELM at the time being'
-        write(fates_log(),*) 'Please make sure the parameter fates_lu_logging_export_frac = 0'
-        call endrun(msg=errMsg(sourcefile, __LINE__))
-
-     
      ! Same for phosphorus
      if (any(EDPftvarcon_inst%prescribed_puptake(:) < -nearzero ) .or. &
           any(EDPftvarcon_inst%prescribed_puptake(:) > 10._r8 )) then
