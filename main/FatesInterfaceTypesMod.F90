1bb4423b38b221eff548ef016147968b0fdb3601
Ryan Knox <rgknox@gmail.com>
2022-06-23 10:41:23 -0400

    Removed duplicate entry of the interface variables hrv_deadstemc_to_prodc

--- main/FatesInterfaceTypesMod.F90
+++ main/FatesInterfaceTypesMod.F90
@@ -725,10 +725,6 @@ module FatesInterfaceTypesMod
       ! FATES LULCC
       real(r8) :: hrv_deadstemc_to_prod10c   ! Harvested C flux to 10-yr wood product pool [Site-Level, gC m-2 s-1]
       real(r8) :: hrv_deadstemc_to_prod100c  ! Harvested C flux to 100-yr wood product pool [Site-Level, gC m-2 s-1]
-
-      ! FATES LULCC
-      real(r8) :: hrv_deadstemc_to_prod10c   ! Harvested C flux to 10-yr wood product pool [Site-Level, gC m-2 s-1]
-      real(r8) :: hrv_deadstemc_to_prod100c  ! Harvested C flux to 100-yr wood product pool [Site-Level, gC m-2 s-1]
       
    end type bc_out_type
 
