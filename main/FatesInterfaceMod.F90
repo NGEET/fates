ae40104e214c8d797b2dc146f89da585bcc2b2d6
Ryan Knox <rgknox@lbl.gov>
2022-09-20 14:41:38 -0400

    Added user name checks for damage mode and parteh 2

--- main/FatesInterfaceMod.F90
+++ main/FatesInterfaceMod.F90
@@ -1536,6 +1536,14 @@ contains
          if(hlm_use_tree_damage .eq. unset_int) then
             write(fates_log(),*) 'FATES dimension/parameter unset: hlm_use_tree_damage, exiting'
             call endrun(msg=errMsg(sourcefile, __LINE__))
+         else
+            if((hlm_use_tree_damage .eq. itrue) .and. &
+                 (hlm_parteh_mode .eq. prt_cnp_flex_allom_hyp))then
+               write(fates_log(),*) 'FATES tree damage is not (yet) compatible with CNP allocation (fates_parteh_mode = 2)'
+               call endrun(msg=errMsg(sourcefile, __LINE__))
+         end if
+
+            
          end if
 
          if(hlm_nitrogen_spec .eq. unset_int) then
