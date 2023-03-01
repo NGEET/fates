fda4aff84f0d0c0173e39ccfaa808255dc5c7b17
Gregory Lemieux <glemieux@users.noreply.github.com>
2023-03-01 13:57:52 -0800

    Add function to update luh2 time units

--- tools/luh2.py
+++ tools/luh2.py
@@ -1,23 +1,59 @@
 #!/usr/bin/env python3
-#
-import xarray as xr
-import pynco
 
-def importdata(inputarg):
+import re
+import xarray as xr
+from nco import Nco
+from nco.custom import Atted
+
+# Import luh2 data
+def importdata(inputarg,datasetout):
 
     # Open files
     # Check to see if a ValueError is raised which is likely due
     # to the LUH2 time units being undecodable by cftime module
     try:
-       ds = xr.open_dataset(inputarg)
+       datasetout = xr.open_dataset(inputarg)
     except ValueError as err:
        print("ValueError:", err)
        errmsg = "User direction: If error is due to units being 'years since ...' " \
                 "update the input data file to change to 'common_years since...'. " \
-                "This can be done using the NCO tool `ncatted`."
+                "This can be done using the luh2.attribupdate function."
        print()
        print(errmsg)
        # exit(2)
+
+# Modify the luh2 metadata to enable xarray to read in data
+# This issue here is that the luh2 time units start prior to
+#
+def attribupdate(inputarg,outputarg):
+
+    nco = Nco()
+
+    # Get the 'time:units' string from the input using ncks
+    timeunitstr = nco.ncks(input=inputarg,variable="time",options=["-m"]).decode()
+
+    # Grab the units string and replace "years" with "common_years"
+    substr = re.search('time:units.*".*"',timeunitstr)
+    newstr = substr.group().replace("time:units = \"years","\"common_years")
+
+    # Use ncatted to update the time units
+    att = "units"
+    var = "time"
+    att_type = "c"
+    opts = [" -a {0},{1},o,{2},{3}".format(att, var, att_type, newstr)]
+    nco.ncatted(input=inputarg, output=outputarg, options=opts)
+
+    # The following is fixed with PR #62 for pynco but isn't in that latest update yet
+    # on conda
+    # nco.ncatted(input=inputarg,output=outputarg,options=[
+    #     Atted(mode="overwrite",
+    #           att_name="units",
+    #           var_name="time",
+    #           value=newstr
+    #           stype="c"
+    #           ),
+    # ])
+
 
 # Close files
 # management_ds.close()
