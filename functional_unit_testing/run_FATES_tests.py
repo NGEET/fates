915a0e1d35d9095c5cb016893242b83bb1c0b621
adrifoster <adrifost@ucar.edu>
2023-05-02 12:17:40 -0600

    update unit test

--- functional_unit_testing/run_FATES_tests.py
+++ functional_unit_testing/run_FATES_tests.py
@@ -12,24 +12,24 @@ from CIME.utils import run_cmd_no_fail
 
 from shutil import rmtree, copy
 
-def main():
+def main(build_dir):
   os.chdir("..")
-  if os.path.isdir("build"):
-    rmtree("build")
+  if os.path.isdir(build_dir):
+    rmtree(build_dir)
 
   ## super hacky right now!!
   run_command = ["../../cime/scripts/fortran_unit_testing/run_tests.py",
-                 "--build-dir", "build"]
+                 "--build-dir", "build.temp"]
   run_cmd_no_fail(" ".join(run_command), combine_output=True)
   
-  copy("build/__command_line_test__/__command_line_test__/unit_testing/test/radiation_test/FATES_rad_test",
-           "unit_testing/test/radiation_test/")
+  copy(os.path.join(build_dir, "__command_line_test__/__command_line_test__/fates_biogeophys_test/solar_rad_test/FATES_rad_exe"),
+           "biogeophys/test/solar_rad_test/")
   
-  os.chdir("unit_testing/test/radiation_test")
-  run_command = ["./FATES_rad_test"]
+  os.chdir("biogeophys/test/solar_rad_test")
+  run_command = ["./FATES_rad_exe"]
   output = run_cmd_no_fail(" ".join(run_command), combine_output=True)
   print(output)
   
   
 if __name__ == "__main__":
-  main()
+  main("build.temp")
