ac181bc9bd605fdf1162da9ef96c23c8338757c2
Gregory Lemieux <glemieux@users.noreply.github.com>
2023-05-04 10:03:14 -0700

    refactor land use mapping

--- biogeochem/FatesLandUseChangeMod.F90
+++ biogeochem/FatesLandUseChangeMod.F90
@@ -9,6 +9,7 @@ module FatesLandUseChangeMod
   use FatesGlobals              , only : endrun => fates_endrun
   use FatesConstantsMod         , only : r8 => fates_r8
   use FatesConstantsMod         , only : itrue, ifalse
+  use FatesConstantsMod         , only : fates_unset_int
   use FatesInterfaceTypesMod    , only : bc_in_type
   use FatesInterfaceTypesMod    , only : hlm_use_luh
   use FatesInterfaceTypesMod    , only : hlm_num_luh2_states
@@ -25,16 +26,32 @@ module FatesLandUseChangeMod
 
   character(len=*), parameter :: sourcefile = __FILE__
 
-  !
   public :: get_landuse_transition_rates
-  public :: init_luh2_fates_mapping
   public :: get_landusechange_rules
   public :: get_luh_statedata
 
 
   ! module data
   integer, parameter :: max_luh2_types_per_fates_lu_type = 5
-  CHARACTER(len=5), protected, DIMENSION(n_landuse_cats,max_luh2_types_per_fates_lu_type) :: luh2_fates_luype_map
+
+  ! Define the mapping from the luh2 state names to the aggregated fates land use categories
+  type :: luh2_fates_lutype_map
+
+     character(len=5), dimension(12) :: state_names = &
+                   [character(len=5) ::  'primf','primn','secdf','secdn', &
+                                         'pastr','range', 'urban', &
+                                         'c3ann','c4ann','c3per','c4per','c3nfx']
+     integer, dimension(12) :: landuse_categories = &
+                                [primaryland, primaryland, secondaryland, secondaryland, &
+                                 pastureland, rangeland, fates_unset_int, &
+                                 cropland, cropland, cropland, cropland, cropland]
+
+     contains
+
+       ! procedure :: Init => init_luh2_fates_mapping
+       procedure :: GetIndex => GetLUH2CategoryFromStateName
+
+  end type luh2_fates_lutype_map
 
 
   ! 03/10/2023 Created By Charlie Koven
@@ -55,6 +72,7 @@ contains
     real(r8), intent(inout) :: landuse_transition_matrix(n_landuse_cats, n_landuse_cats)  ! [m2/m2/year]
 
     ! !LOCAL VARIABLES:
+    type(luh2_fates_lutype_map) :: lumap
     integer :: i_donor, i_receiver, i_luh2_transitions, i_luh2_states
     character(5) :: donor_name, receiver_name
     character(14) :: transition_name
@@ -67,70 +85,41 @@ contains
        
        !!may need some logic here to ask whether or not ot perform land use cahnge on this timestep. current code occurs every day.
        
-       ! identify urban fraction so that it can be removed.
-       urban_fraction = 0._r8
-       do i_luh2_states = 1, hlm_num_luh2_states
-          if (bc_in%hlm_luh_state_names(i_luh2_states) .eq. 'urban') then
-             urban_fraction = bc_in%hlm_luh_states(i_luh2_states)
+       ! identify urban fraction so that it can be accounted for in the fates land use aggregation
+       urban_fraction = bc_in%hlm_luh_states(findloc(bc_in%hlm_luh_state_names,'urban',dim=1))
+
+       transitions_loop: do i_luh2_transitions = 1, hlm_num_luh2_transitions
+
+          ! transition names are written in form xxxxx_to_yyyyy where x and y are donor and receiver state names
+          transition_name = bc_in%hlm_luh_transition_names(i_luh2_transitions)
+          donor_name = transition_name(1:5)
+          receiver_name = transition_name(10:14)
+
+          ! Get the fates land use type index associated with the luh2 state types
+          i_donor= lumap%GetIndex(donor_name)
+          i_receiver = lumap%GetIndex(receiver_name)
+
+          ! Avoid transitions with 'urban' as those are handled seperately
+          if (i_donor .ne. fates_unset_int .or. i_receiver .ne. fates_unset_int) then
+             landuse_transition_matrix(i_donor,i_receiver) = &
+                  landuse_transition_matrix(i_donor,i_receiver) +  bc_in%hlm_luh_transitions(i_luh2_transitions) / (1._r8 - urban_fraction)
+
           end if
-       end do
-    
-       ! loop over FATES donor and receiver land use types
-       donor_loop: do i_donor = 1,n_landuse_cats
-          receiver_loop: do i_receiver = 1,n_landuse_cats
-
-             ! ignore diagonals of transition matrix
-             not_diagonal: if ( i_donor .ne. i_receiver ) then
-
-                ! ignore special case of primary -> secondary, which is handled by harvest mechanism
-                not_primary_to_secondary: if ( .not. ((i_donor .eq. primaryland) .and. (i_receiver .eq. secondaryland)) ) then
-
-                   transitions_loop: do i_luh2_transitions = 1, hlm_num_luh2_transitions
-
-                      ! transition names are written in form xxxxx_to_yyyyy where x and y are donor and receiver state names
-                      transition_name = bc_in%hlm_luh_transition_names(i_luh2_transitions)
-                      donor_name = transition_name(1:5)
-                      receiver_name = transition_name(10:14)
-
-                      if (any(luh2_fates_luype_map(:,i_donor) == donor_name) .and. &
-                           any(luh2_fates_luype_map(:,i_receiver) == receiver_name)) then
-
-                         landuse_transition_matrix(i_donor,i_receiver) = &
-                              landuse_transition_matrix(i_donor,i_receiver) +  bc_in%hlm_luh_transitions(i_luh2_transitions) / (1._r8 - urban_fraction)
-
-                      end if
-                   end do transitions_loop
-                end if not_primary_to_secondary
-             end if not_diagonal
-          end do receiver_loop
-       end do donor_loop
+       end do transitions_loop
     end if use_luh_if
   end subroutine get_landuse_transition_rates
 
   !----------------------------------------------------------------------------------------------------
 
-  subroutine init_luh2_fates_mapping
+  function GetLUH2CategoryFromStateName(this, state_name) result(landuse_category)
 
-    ! initialize the character mapping of the LUH2 : FATES correspondance
-    luh2_fates_luype_map(:,:) = ''
-    
-    luh2_fates_luype_map(1,primaryland) = 'primf'
-    luh2_fates_luype_map(2,primaryland) = 'primn'
+    class(luh2_fates_lutype_map) :: this
+    character(len=5), intent(in) :: state_name
+    integer :: landuse_category
 
-    luh2_fates_luype_map(1, secondaryland) = 'secdf'
-    luh2_fates_luype_map(2, secondaryland) = 'secdn'
+    landuse_category = this%landuse_categories(findloc(this%state_names,state_name,dim=1))
 
-    luh2_fates_luype_map(1,cropland) = 'c3ann'
-    luh2_fates_luype_map(2,cropland) = 'c4ann'
-    luh2_fates_luype_map(3,cropland) = 'c3per'
-    luh2_fates_luype_map(4,cropland) = 'c4per'
-    luh2_fates_luype_map(5,cropland) = 'c3nfx'
-
-    luh2_fates_luype_map(1,pastureland) = 'pastr'
-
-    luh2_fates_luype_map(1,rangeland) = 'range'
-    
-  end subroutine init_luh2_fates_mapping
+  end function GetLUH2CategoryFromStateName
 
   !----------------------------------------------------------------------------------------------------
 
@@ -221,6 +210,9 @@ contains
 
     type(bc_in_type) , intent(in) :: bc_in
     real(r8), intent(out) :: state_vector(n_landuse_cats)  ! [m2/m2]
+
+    ! LOCALS
+    type(luh2_fates_lutype_map) :: lumap
     real(r8) :: urban_fraction
     integer  :: i_luh2_states
     integer  :: ii
@@ -229,23 +221,23 @@ contains
     ! zero state vector
     state_vector(:) = 0._r8
 
-    ! identify urban fraction so that it can be removed.
-    urban_fraction = 0._r8
-    do i_luh2_states = 1, hlm_num_luh2_states
-       if (bc_in%hlm_luh_state_names(i_luh2_states) .eq. 'urban') then
-          urban_fraction = bc_in%hlm_luh_states(i_luh2_states)
-       end if
-    end do
+    ! identify urban fraction so that it can be factored into the land use state output
+    urban_fraction = bc_in%hlm_luh_states(findloc(bc_in%hlm_luh_state_names,'urban',dim=1))
 
     ! loop over all states and add up the ones that correspond to a given fates land use type
     do i_luh2_states = 1, hlm_num_luh2_states
+
+       ! Get the luh2 state name and determine fates aggregated land use
+       ! type index from the state to lutype map
        state_name = bc_in%hlm_luh_state_names(i_luh2_states)
-       do ii = 1, max_luh2_types_per_fates_lu_type
-          if (state_name .eq. luh2_fates_luype_map(i_luh2_states, ii)) then
-             state_vector(i_luh2_states) = state_vector(i_luh2_states) + &
-                  bc_in%hlm_luh_states(i_luh2_states) / (1._r8 - urban_fraction)
-          end if
-       end do
+       ii = lumap%GetIndex(state_name)
+
+      ! Avoid 'urban' states whose indices have been given unset values
+      if (ii .ne. fates_unset_int) then
+        state_vector(ii) = state_vector(ii) + &
+            bc_in%hlm_luh_states(i_luh2_states) / (1._r8 - urban_fraction)
+       end if
+       ! end do
     end do
 
     ! check to ensure total area == 1, and correct if not
