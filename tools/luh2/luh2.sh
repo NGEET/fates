35dee92f297767b5ecc6753cac609942320dd2d1
Gregory Lemieux <glemieux@users.noreply.github.com>
2023-07-24 15:22:32 -0600

    reworking luh2 shell script call

--- tools/luh2/luh2.sh
+++ tools/luh2/luh2.sh
@@ -7,54 +7,47 @@
 # of the luh2 data and the dataset to regrid against
 
 # LUH2 data names
-export DATA_LOC=$1
-export STATES_FILE=states.nc
-export TRANSITIONS_FILE=transitions.nc
-export MANAGE_FILE=management.nc
-export STATIC_FILE=staticData_quarterdeg.nc
-export REGRID_TARGET_FILE=surfdata_4x5_16pfts_Irrig_CMIP6_simyr2000_c170824.nc
+DATA_LOC=$1
+TARGET_LOC=$2
+OUTPUT_LOC=$3
+STATES_FILE=states.nc
+TRANSITIONS_FILE=transitions.nc
+MANAGE_FILE=management.nc
+STATIC_FILE=staticData_quarterdeg.nc
+REGRID_TARGET_FILE=surfdata_4x5_16pfts_Irrig_CMIP6_simyr2000_c170824.nc
 
 # Save files
-export REGRID_SAVE=regridder.nc
-export OUTPUT=LUH2_historical_0850_2015_4x5.nc
+REGRID_SAVE=regridder.nc
+OUTPUT_FILE=LUH2_historical_0850_2015_4x5.nc
 
 # Combine strings
-export STATES=${DATA_LOC}/${STATES_FILE}
-export TRANSITIONS=${DATA_LOC}/${TRANSITIONS_FILE}
-export MANAGE=${DATA_LOC}/${MANAGE_FILE}
-export STATIC=${DATA_LOC}/${STATIC_FILE}
-export REGRID_TARGET=${DATA_LOC}/${REGRID_TARGET_FILE}
-export REGRIDDER=${DATA_LOC}/${REGRID_SAVE}
+STATES=${DATA_LOC}/${STATES_FILE}
+TRANSITIONS=${DATA_LOC}/${TRANSITIONS_FILE}
+MANAGE=${DATA_LOC}/${MANAGE_FILE}
+STATIC=${DATA_LOC}/${STATIC_FILE}
+REGRID_TARGET=${TARGET_LOC}/${REGRID_TARGET_FILE}
+REGRIDDER=${OUTPUT_LOC}/${REGRID_SAVE}
 
-# Create copies of the luh2 data that have the time data modified
 # Comment this out if the user already has the modified datasets available
-
-# Update that filename to point to
 
 # Regrid the luh2 data against a target surface data set and then remove the states_modified file
 echo "starting storage"
-du -h ${DATA_LOC}
-export STATES=${DATA_LOC}/states_modified.nc
-python luh2.py  -l ${STATES} -s ${STATIC} -r ${REGRID_TARGET} -w ${REGRIDDER} -o ${DATA_LOC}/states_regrid.nc
+du -h ${OUTPUT_LOC}
+python luh2.py  -l ${STATES} -s ${STATIC} -r ${REGRID_TARGET} -w ${REGRIDDER} -o ${OUTPUT_LOC}/states_regrid.nc
 echo -e"storage status:\n"
-du -h ${DATA_LOC}
-rm ${STATES}
+du -h ${OUTPUT_LOC}
 
 # Regrid the luh2 transitions data using the saved regridder weights file and merge into previous regrid output
-export TRANSITIONS=${DATA_LOC}/transitions_modified.nc
 python luh2.py  -l ${TRANSITIONS} -s ${STATIC} -r ${REGRID_TARGET} -w ${REGRIDDER} \
-                -m ${DATA_LOC}/states_regrid.nc -o ${DATA_LOC}/states_trans_regrid.nc
+                -m ${OUTPUT_LOC}/states_regrid.nc -o ${OUTPUT_LOC}/states_trans_regrid.nc
 echo -e"storage status:\n"
-du -h ${DATA_LOC}
-rm ${TRANSITIONS}
+du -h ${OUTPUT_LOC}
 rm ${DATA_LOC}/states_regrid.nc
 
 # Regrid the luh2 management data using the saved regridder file and merge into previous regrid output
-export MANAGE=${DATA_LOC}/management_modified.nc
 python luh2.py  -l ${MANAGE} -s ${STATIC} -r ${REGRID_TARGET} -w ${REGRIDDER} \
-                -m ${DATA_LOC}/states_trans_regrid.nc -o ${OUTPUT}
+                -m ${OUTPUT_LOC}/states_trans_regrid.nc -o ${OUTPUT_LOC}/${OUTPUT_FILE}
 echo -e"storage status:\n"
-du -h ${DATA_LOC}
-rm ${MANAGE}
-rm ${DATA_LOC}/states_trans_regrid.nc
+du -h ${OUTPUT_LOC}
+rm ${OUTPUT_LOC}/states_trans_regrid.nc
 rm ${REGRIDDER}
