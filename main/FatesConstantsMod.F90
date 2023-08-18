0630320a16b76d4cf804d2670eea971ea2d97eaa
adrifoster <adrifost@ucar.edu>
2023-08-18 09:24:58 -0600

    update error tolerance

--- main/FatesConstantsMod.F90
+++ main/FatesConstantsMod.F90
@@ -148,7 +148,7 @@ integer, parameter, public :: isemi_stress_decid = 2 ! If the PFT is stress (dro
 
   ! area tolerance checks
   real(fates_r8), parameter, public :: area_error_1     = 1.0e-16_fates_r8 ! error tolerance for area checks (canopy, patch)
-  real(fates_r8), parameter, public :: area_error_2     = 1.0e-12_fates_r8 ! error tolerance for tree lai checks
+  real(fates_r8), parameter, public :: area_error_2     = 1.0e-11_fates_r8 ! error tolerance for tree lai checks
   real(fates_r8), parameter, public :: area_error_3     = 10.e-9_fates_r8  ! error tolerance for area checks (canopy, patch)
   real(fates_r8), parameter, public :: area_error_4     = 1.0e-10_fates_r8 ! error tolerance for area checks
 
