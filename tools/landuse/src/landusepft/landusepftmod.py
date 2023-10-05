6b71decd4f28418765c8897008c82f3540446831
Gregory Lemieux <glemieux@users.noreply.github.com>
2023-10-05 16:53:29 -0700

    Rename landusepft to landusepftmod
    All tests pass

--- tools/landuse/src/landusepft/landusepftmod.py
+++ tools/landuse/src/landusepft/landusepftmod.py
@@ -45,6 +45,7 @@ def DefineMask(dataset):
     else:
         return mask
 
+# Renormalize the pft percentages without the bareground
 def RenormalizePFTs(dataset, mask):
     # Remove the bareground pft index from the dataset
     percent = dataset.PCT_NAT_PFT.isel(natpft=slice(1,None))
@@ -53,12 +54,6 @@ def RenormalizePFTs(dataset, mask):
     return percent
 
 # Steps
-# - import clm landuse-pft data (1/4 degree)
-# - import luh2 static data file (1/4 degree)
-# - set the mask based on static data file `icwtr` data
-# - calculate the percentage of forest, pasture, and other (i.e. range) pft percentages
-# - calculate the primary and secondard forest percent using the static data `fstnf`
-# primary_secondary_percent = luh2_staticdata.fstnf * forest_pft_percent + (1.- luh2_staticdata.fstnf) * other_pft_percent
 # - concatenate all this information together (including mask)
 # - add/adjust lat/lon names and time input for regridding
 # - regrid using luh2mod.py tooling
--- tools/landuse/tests/test_landusepft.py
+++ tools/landuse/tests/test_landusepft.py
@@ -1,11 +1,11 @@
 import pytest
 import numpy as np
 
-from landusepft import landusepft
+from landusepft import landusepftmod
 
 # Postive test case for importing LUH2 static data file
 def test_posImportStaticLUH2File(static_file_location):
-    data = landusepft.ImportStaticLUH2File(static_file_location)
+    data = landusepftmod.ImportStaticLUH2File(static_file_location)
     static_variables = list(data.var())
     assert static_variables == ['ptbio',
                                 'fstnf',
@@ -18,13 +18,13 @@ def test_posImportStaticLUH2File(static_file_location):
 # Negative test case for importing incorrect file via static luh2 file open function
 def test_negImportStaticLUH2File(landusepft_file_location):
     with pytest.raises(TypeError) as exp:
-        landusepft.ImportStaticLUH2File(landusepft_file_location)
+        landusepftmod.ImportStaticLUH2File(landusepft_file_location)
     assert str(exp.value) == "incorrect file, must be LUH2 static file"
 
 # Positive test case for importing landuse x pft data file
 # TODO: parameterize this call to run all the landusepft files
 def test_posImportLandusePFTFile(landusepft_file_location):
-    data = landusepft.ImportLandusePFTFile(landusepft_file_location)
+    data = landusepftmod.ImportLandusePFTFile(landusepft_file_location)
     landusepft_variables = list(data.var())
     assert landusepft_variables == ['EDGEN',
                                    'EDGEE',
@@ -42,23 +42,23 @@ def test_posImportLandusePFTFile(landusepft_file_location):
 # Negative test case for importing incorrect file via CLM5 landuse file open function
 def test_negImportLandusePFTFile(static_file_location):
     with pytest.raises(TypeError) as exp:
-        landusepft.ImportLandusePFTFile(static_file_location)
+        landusepftmod.ImportLandusePFTFile(static_file_location)
     assert str(exp.value) == "incorrect file, must be CLM5 landuse file"
 
 # Confirm that the data type has been converted upon import from single to double precision
 def test_pos_ImportLandusePFTFileDtypeConversion(landusepft_file_location):
-    dataset = landusepft.ImportLandusePFTFile(landusepft_file_location)
+    dataset = landusepftmod.ImportLandusePFTFile(landusepft_file_location)
     assert np.dtype('float64') == dataset['PCT_NAT_PFT'].dtype
 
 # Unit tests to update CLM5 landuse datasets with lat/lon coordinates
 def test_latlon_posAddLatLonCoordinates(landusepft_dataset):
-    dataset_modified = landusepft.AddLatLonCoordinates(landusepft_dataset)
+    dataset_modified = landusepftmod.AddLatLonCoordinates(landusepft_dataset)
     assert 'lat' in list(dataset_modified.coords) and 'lon' in list(dataset_modified.coords)
 
 # Test that the math of the function makes sense.  We can sum that lat/lon variables
 # to makes sure that the update is correct
 def test_latlonsum_posAddLatLonCoordinates(landusepft_dataset):
-    dataset_modified = landusepft.AddLatLonCoordinates(landusepft_dataset)
+    dataset_modified = landusepftmod.AddLatLonCoordinates(landusepft_dataset)
     assert (sum(abs(dataset_modified.lon)).values.item() == 129600.
            and sum(abs(dataset_modified.lat)).values.item() == 32400.)
 
@@ -69,29 +69,29 @@ def test_latlonsum_posAddLatLonCoordinates(landusepft_dataset):
 
 # Make sure that the generated mask makes sense in that a known gridcell is nan
 def test_posDefineMask(static_dataset):
-    maskoutput = landusepft.DefineMask(static_dataset)
+    maskoutput = landusepftmod.DefineMask(static_dataset)
     assert np.isnan(maskoutput[0][0])
 
 # Make sure that the generated mask is equal to 1 where it isn't nan
 def test_pos_ones_DefineMask(static_dataset):
-    maskoutput = landusepft.DefineMask(static_dataset)
+    maskoutput = landusepftmod.DefineMask(static_dataset)
     assert (maskoutput.to_dataframe().dropna() == 1.).all().values.item()
 
 # Make sure a known gridcell that should be land is not nan
 # TODO: change this to use sel indexing
 def test_neg_output_DefineMask(static_dataset):
-    maskoutput = landusepft.DefineMask(static_dataset)
+    maskoutput = landusepftmod.DefineMask(static_dataset)
     assert not(np.isnan(maskoutput[360][850]))
 
 def test_neg_input_DefineMask(landusepft_dataset):
     with pytest.raises(AttributeError) as exp:
-        maskoutput = landusepft.DefineMask(landusepft_dataset)
+        maskoutput = landusepftmod.DefineMask(landusepft_dataset)
     assert str(exp.value) == "incorrect dataset, must be static luh2 dataset"
 
 # Bare ground fraction removal unit test
 # Make sure that the pft fractions sum to 100% on a known land gridcell
 def test_posRenormalizePFTs(landusepft_dataset,mock_mask):
-    percent = landusepft.RenormalizePFTs(landusepft_dataset, mock_mask)
+    percent = landusepftmod.RenormalizePFTs(landusepft_dataset, mock_mask)
 
     # Sum along the natpft dimension only.  Use min_count to avoid
     # NaNs from masking turning into zeros
@@ -107,23 +107,3 @@ def test_posRenormalizePFTs(landusepft_dataset,mock_mask):
     # Check that all the summations are unity within a specific tolerance
     tolerance = 3.0e-16
     assert (abs(percent - 1.0) < tolerance).all().values.item()
-
-# @pytest.mark.skip(reason="this needs more work")
-# Negative test case for importing the LUH2 static data file
-# def test_negImportStaticLUH2File(mockluh2_file_location):
-#     with pytest.raises(ValueError) as exp:
-#         data = landusepft.ImportStaticLUH2File(mockluh2_file_location)
-#         variables = list(data.var())
-#     assert str(exp.value) == "'Error: file imported is not the LUH2 static data file'"
-
-# def test_negImportFile(static_file_location):
-#     with pytest.raises(AttributeError) as exp:
-#         data = landusepft.ImportStaticLUH2File(static_file_location)
-#         static_variables = list(data.var())
-#     assert str(exp.value) == "'list' object has no attribute 'var'"
-
-# Possible tests
-# - Check that import fails if argument is not given the appropriate landuse pft data
-# - Check that import fails if argument is not given the appropriate static file data
-# - Check by assertion that mask value is zero over specific points on a 1/4 deg grid?
-# - Check by assertion that sum of certain land use and/or pft combinations sum to 100?
