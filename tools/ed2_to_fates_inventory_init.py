c541a74d4ca57492ce146ea66ddc2a46c79be1d8
jessica needham <jessiefneedham@gmail.com>
2023-10-19 10:18:04 -0700

    clean up python script -make simpler

--- tools/ed2_to_fates_inventory_init.py
+++ tools/ed2_to_fates_inventory_init.py
@@ -2,9 +2,7 @@
 
 ### This script takes a ED2 style inventory init file and converts it to a file compatible with FATES.
 # It accepts the following flags:
-# --type : patch or cohort
 # --fin : input filename
-# --fout : output file name
 
 import argparse
 import pandas as pd
@@ -13,23 +11,28 @@ import sys
 def main():
     parser = argparse.ArgumentParser(description='Parse command line arguments to this script.')
     #
-    parser.add_argument('--type', dest='fatestype', type=str, help="patch or cohort. Required.", required=True)
     parser.add_argument('--fin', dest='fnamein', type=str, help="Input filename.  Required.", required=True)
-    parser.add_argument('--fout', dest='fnameout', type=str, help="Output filename.  Required.", required=True) 
-
+  
     args = parser.parse_args()
+
+    # is it a pss or css file?
+    filetype  = args.fnamein.split('.')[1]
+
+    # make the new file name
+    base_filename = args.fnamein.split('.')[0]
+    output_filename = f"{base_filename}_{'fates'}.{filetype}"
 
     # open the input data
     dsin = pd.read_csv(args.fnamein, delim_whitespace=True)
 
     # if patch file delete unnecessary patch columns
-    if args.fatestype == 'patch' :
+    if filetype == 'pss' :
         keep_col = ['time', 'patch', 'trk', 'age', 'area']
         newds = dsin[keep_col]
         
 
     # if cohort file delete unnecessary cohort columns
-    elif args.fatestype == 'cohort' :
+    elif filetype == 'css' :
         keep_col = ['time', 'patch', 'dbh', 'pft', 'nplant']
         newds = dsin[keep_col]
         
@@ -37,7 +40,7 @@ def main():
         print("type must be one of patch or cohort")
 
 
-    newds.to_csv(args.fnameout , index=False, sep=' ')    
+    newds.to_csv(output_filename, index=False, sep=' ')    
 # ========================================================================================================
 # This is the actual call to main
 
