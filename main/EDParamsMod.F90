63c2c47698522402119602537c1a85ea0a6519f4
jessica needham <jessiefneedham@gmail.com>
2023-11-04 16:54:12 -0700

    revert nlevleaf to 30

--- main/EDParamsMod.F90
+++ main/EDParamsMod.F90
@@ -100,7 +100,7 @@ module EDParamsMod
    integer, parameter, public :: nclmax = 2                ! Maximum number of canopy layers
 
    ! parameters that govern the VAI (LAI+SAI) bins used in radiative transfer code
-   integer, parameter, public :: nlevleaf = 40   ! number of leaf+stem layers in each canopy layer
+   integer, parameter, public :: nlevleaf = 30   ! number of leaf+stem layers in each canopy layer
 
    real(r8), public :: dinc_vai(nlevleaf)   = fates_unset_r8 ! VAI bin widths array
    real(r8), public :: dlower_vai(nlevleaf) = fates_unset_r8 ! lower edges of VAI bins
