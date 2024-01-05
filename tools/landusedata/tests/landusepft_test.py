a25a1277a40ddd8956ba5c38025b5b02001df104
Gregory Lemieux <glemieux@users.noreply.github.com>
2024-01-05 13:15:53 -0800

    move luh2 file import test into luh2 test module

--- tools/landusedata/tests/landusepft_test.py
+++ tools/landusedata/tests/landusepft_test.py
@@ -3,24 +3,6 @@ import numpy as np
 
 from landusedata import landusepftmod
 
-# Postive test case for importing LUH2 static data file
-def test_posImportStaticLUH2File(static_file_location):
-    data = landusepftmod.ImportStaticLUH2File(static_file_location)
-    static_variables = list(data.var())
-    assert static_variables == ['ptbio',
-                                'fstnf',
-                                'carea',
-                                'icwtr',
-                                'ccode',
-                                'lat_bounds',
-                                'lon_bounds']
-
-# Negative test case for importing incorrect file via static luh2 file open function
-def test_negImportStaticLUH2File(landusepft_file_location):
-    with pytest.raises(TypeError) as exp:
-        landusepftmod.ImportStaticLUH2File(landusepft_file_location)
-    assert str(exp.value) == "incorrect file, must be LUH2 static file"
-
 # Positive test case for importing landuse x pft data file
 # TODO: parameterize this call to run all the landusepft files
 def test_posImportLandusePFTFile(landusepft_file_location):
---
+++ tools/landusedata/tests/luh2_test.py
@@ -0,0 +1,22 @@
+import pytest
+
+# from landusedata import luh2mod
+from landusedata import landusepftmod as luh2mod
+
+# Postive test case for importing LUH2 static data file
+def test_posImportStaticLUH2File(static_file_location):
+    data = luh2mod.ImportStaticLUH2File(static_file_location)
+    static_variables = list(data.var())
+    assert static_variables == ['ptbio',
+                                'fstnf',
+                                'carea',
+                                'icwtr',
+                                'ccode',
+                                'lat_bounds',
+                                'lon_bounds']
+
+# Negative test case for importing incorrect file via static luh2 file open function
+def test_negImportStaticLUH2File(landusepft_file_location):
+    with pytest.raises(TypeError) as exp:
+        luh2mod.ImportStaticLUH2File(landusepft_file_location)
+    assert str(exp.value) == "incorrect file, must be LUH2 static file"
