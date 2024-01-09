81040408fd8c3c7d5f2dc004f447b4386617ae49
Gregory Lemieux <glemieux@users.noreply.github.com>
2024-01-09 15:08:25 -0800

    add lat/lon coordinate add to target file prep function

--- tools/landusedata/src/landusedata/utils.py
+++ tools/landusedata/src/landusedata/utils.py
@@ -1,11 +1,15 @@
-import xarray as xr
+import xarray
 
-def RenameLatLon(regrid_target):
+def RegridTargetPrep(regrid_target):
     """
     Rename the CLM surface data file regrid target lat/lon dimensions
 
     The CLM surface datasets use a dimensions name that is not recognizable
-    by xesmf.  As such, this function renames the dimensions.
+    by xesmf.  As such, this function renames the dimensions.  It also adds
+    lat/lon coordinates based on the LONGXY and LATIXY variables.
     """
     ds = regrid_target.rename_dims(dims_dict={'lsmlat':'lat','lsmlon':'lon'})
+    ds['lon'] = ds.LONGXY.isel(lat=0)
+    ds['lat'] = ds.LATIXY.isel(lon=0)
+
     return ds
--- tools/landusedata/tests/utils_test.py
+++ tools/landusedata/tests/utils_test.py
@@ -2,10 +2,18 @@ import pytest
 
 from landusedata import utils
 
-def test_RenameLatLon(target_dataset):
-    target_dataset = utils.RenameLatLon(target_dataset)
+def test_RegridTargetPrep_dims(target_dataset):
+    target_dataset = utils.RegridTargetPrep(target_dataset)
     dim_list = list(target_dataset.dims)
     latlon_list = ['lat','lon']
     test_list = [dim for latlon in latlon_list for dim in dim_list
                  if latlon in dim]
+    assert latlon_list == test_list
+
+def test_RegridTargetPrep_coords(target_dataset):
+    target_dataset = utils.RegridTargetPrep(target_dataset)
+    coords_list = list(target_dataset.coords)
+    latlon_list = ['lat','lon']
+    test_list = [coord for latlon in latlon_list for coord in coords_list
+                 if latlon in coord]
     assert latlon_list == test_list
