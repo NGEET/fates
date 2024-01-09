e4d350a1ecbac60b0bd08b30f346b1541cea2e4c
Gregory Lemieux <glemieux@users.noreply.github.com>
2024-01-08 16:57:36 -0800

    transfer the luh2 argument options to landusedata tool main

--- tools/landusedata/src/landusedata/_main.py
+++ tools/landusedata/src/landusedata/_main.py
@@ -3,48 +3,59 @@ import argparse
 from landusedata.luh2 import luh2print
 from landusedata.landusepft import lupftprint
 
-# Start moving command line arguments to a main package
-# The idea here is that the user is simply going to call a couple of top level options
-# for either creating the luh2 raw data file or the landusepft data file.  Ideally the user
-# will not need to wrap this in a shell script
-# This is something we also want to be testable with pytest
-
 def main(argv=None):
 
-    # Define top level parser and subparser option for luh2 or landuse x pft data tool commands
+    # Define top level parser
     parser = argparse.ArgumentParser(description="FATES landuse data tool")
-    subparsers = parser.add_subparsers(required=True, help='subcommand help')
 
+    # Target regrid file - is there a nice way to share this between subparsers?
+    # parser.add_argument('regrid_target_file', help='target surface data file with desired grid resolution')
 
-    # The user should input the landuse_data_output_type.  Do this to simply
-    # avoid determining what they want from the inputs.
-    luh2_parser = subparsers.add_parser('luh2', help='landuse harmonization timeseries data output')
-    lupft_parser = subparsers.add_parser('lupft', help='landuse x pft static data map output')
-    # parser.add_argument('landuse_type', choices=['luh2','lupft'],
-    #                     help="landuse data file output type to be created, either luh2 timeseries or landuse x pft static mapping")
+    # Define subparser option for luh2 or landuse x pft data tool subcommands
+    subparsers = parser.add_subparsers(required=True, title='subcommands',
+                                       help='landuse data tool subcommand options')
+    luh2_parser = subparsers.add_parser('luh2', prog='luh2',
+                                        help='generate landuse harmonization timeseries data output')
+    lupft_parser = subparsers.add_parser('lupft', prog='lupft',
+                                         help='generate landuse x pft static data map output')
 
     # Set the default called function for the subparser command
     luh2_parser.set_defaults(func=luh2print)
     lupft_parser.set_defaults(func=lupftprint)
 
-    # Target regrid file
-    # Note that this will be the first argument after all the subparser arguments
-    parser.add_argument('regrid_target_file', help='target surface data file with desired grid resolution')
-
-    # The user needs to input specific file locations
-    # Make these grouped based on the landuse_type choice?
-    #
-    # Let the user override the output?
-    #
-    # Let the user truncate the timeseries data
-    # TO DO: using the checking function to report back if invalid file input
-
     # LUH2 subparser arguments
-    luh2_parser.add_argument("luh2_state_file",
-                         help = "full path of luh2 raw states file")
+    luh2_parser.add_argument('regrid_target_file',
+                             help='target surface data file with desired grid resolution')
+    luh2_parser.add_argument('luh2_file',
+                             help = "full path of luh2 raw states file")
+    luh2_parser.add_argument("-s", "--luh2_static_file",
+                        required=True,
+                        help = "luh2 static data file")
+    luh2_parser.add_argument("-r","--regridder_target_file",
+                        required=True,
+                        help = "target file with desired resolution to regrid luh2 data to")
+    luh2_parser.add_argument("-w", "--regridder_weights",
+                        default = 'regridder.nc',
+                        help = "filename of regridder weights to write to or reuse (if -m option used)")
+    luh2_parser.add_argument("-b","--begin",
+                        type = int,
+                        default = None,
+                        help = "beginning of date range of interest")
+    luh2_parser.add_argument("-e","--end",
+                        type = int,
+                        default = None,
+                        help = "ending of date range to slice")
+    luh2_parser.add_argument("-o","--output",
+                        default = 'LUH2_timeseries.nc',
+                        help = "output filename")
+    luh2_parser.add_argument("-m", "--luh2_merge_file",
+                        default = None,
+                        help = "previous luh2 output filename to merge into current run output")
 
     # Landuse x pft subparser arguments
-    lupft_parser.add_argument("lupft_surf_file",
+    lupft_parser.add_argument('regrid_target_file',
+                             help='target surface data file with desired grid resolution')
+    lupft_parser.add_argument('lupft_surf_file',
                          help = "full path of the CLM landuse x pft surface data file")
 
     # Parse the arguments
