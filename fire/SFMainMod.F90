b68e48f58f99aca54a45a056c02a067611ca7d74
adrifoster <adrifost@ucar.edu>
2024-03-18 15:24:22 -0600

    update use statements

--- fire/SFMainMod.F90
+++ fire/SFMainMod.F90
@@ -118,6 +118,7 @@ contains
     use SFParamsMod,       only : SF_val_fdi_a, SF_val_fdi_b
     use FatesConstantsMod, only : tfrz => t_water_freeze_k_1atm
     use FatesConstantsMod, only : sec_per_day, sec_per_min
+    use EDTypesMod,        only : CalculateTreeGrassArea
 
     ! CONSTANTS:
     real(r8), parameter :: wind_atten_treed = 0.4_r8 ! wind attenuation factor for tree fraction
--- fire/SFNesterovMod.F90
+++ fire/SFNesterovMod.F90
@@ -1,6 +1,8 @@
 module SFNesterovMod
 
   use FatesConstantsMod, only : r8 => fates_r8
+  use FatesGlobals,      only : endrun => fates_endrun
+  use FatesGlobals,      only : fates_log
   use SFFireWeatherMod,  only : fire_weather
   
   implicit none
@@ -62,25 +64,44 @@ module SFNesterovMod
     real(r8) function calc_nesterov_index(this, temp_C, precip, rh)
       !
       !  DESCRIPTION:
-      !  Calculates current day's Nesterov Index for a given input values
+      !  Calculates current day's Nesterov Index for given input values
 
-      use SFParamsMod, only : SF_val_fdi_a, SF_val_fdi_b
+      use SFParamsMod,       only : SF_val_fdi_a, SF_val_fdi_b
+      use FatesConstantsMod, only : nearzero
       
       ! ARGUMENTS:
       class(nesterov_index), intent(in) :: this   ! nesterov index extended class
       real(r8),              intent(in) :: temp_C ! daily averaged temperature [degrees C]
       real(r8),              intent(in) :: precip ! daily precipitation [mm]
-      real(r8),              intent(in) :: rh     ! daily relative humidity [rh]
+      real(r8),              intent(in) :: rh     ! daily relative humidity [%]
 
       ! LOCALS:
       real(r8) :: yipsolon ! intermediate variable for dewpoint calculation
       real(r8) :: dewpoint ! dewpoint
 
-      if (precip > min_precip_thresh) then ! NI is 0.0 if it rains
+      ! error checking, if temperature equals -1.0*SF_val_fdi_b parameter, we get a divide
+      ! by zero
+      if (abs(SF_val_fdi_b + temp_C) < nearzero) then
+        write(fates_log(), *) 'SF_val_fdi_b: (', SF_val_fdi_b, ') + temp_C: (', temp_C, ') == 0.0 - divide by zero imminent!'
+        write(fates_log(), *) 'SF_val_fdi_b should be updated using the parameter file.'
+        write(fates_log(), *) 'Otherwise check values for temp_C'
+        call endrun(msg=errMsg(sourcefile, __LINE__))
+      end if 
+
+      if (precip > min_precip_thresh) then ! NI is 0.0 if it rainsf
         calc_nesterov_index = 0.0_r8
       else 
         ! Calculate dewpoint temperature 
-        yipsolon = (SF_val_fdi_a*temp_C)/(SF_val_fdi_b + temp_C) + log(max(1.0_r8, rh)/100.0_r8) 
+        yipsolon = (SF_val_fdi_a*temp_C)/(SF_val_fdi_b + temp_C) + log(max(1.0_r8, rh)/100.0_r8)
+
+        ! error checking, if SF_val_fdi_a parameter - ypisolon == 0, we get a divide
+        ! by zero
+        if (abs(SF_val_fdi_a - yipsolon) < nearzero) then
+          write(fates_log(), *) 'SF_val_fdi_a: (', SF_val_fdi_a, ') - yipsolon: (', yipsolon, ') == 0.0 - divide by zero imminent!'
+          write(fates_log(), *) 'SF_val_fdi_a should be updated using the parameter file.'
+          write(fates_log(), *) 'Otherwise check values for yipsolon'
+          call endrun(msg=errMsg(sourcefile, __LINE__))
+        end if 
         dewpoint = (SF_val_fdi_b*yipsolon)/(SF_val_fdi_a - yipsolon) 
         
         ! Nesterov 1968.  Eq 5, Thonicke et al. 2010
