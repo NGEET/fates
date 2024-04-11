e77dda4614ba644b0188b74acef5fd056547a1da
Gregory Lemieux <glemieux@users.noreply.github.com>
2024-04-11 10:35:49 -0700

    convert loop to single line call
    Also make sure that all primaryland pft areas are exactly zero when
    bareground area is 1

--- main/EDInitMod.F90
+++ main/EDInitMod.F90
@@ -568,13 +568,12 @@ contains
              do i_landusetype = 1, n_landuse_cats
                 sumarea = sum(sites(s)%area_pft(1:numpft,i_landusetype))
                 if(sumarea.gt.nearzero)then
-                   do ft =  1,numpft
-                      sites(s)%area_pft(ft, i_landusetype) = sites(s)%area_pft(ft, i_landusetype)/sumarea
-                   end do !ft
+                      sites(s)%area_pft(:, i_landusetype) = sites(s)%area_pft(:, i_landusetype)/sumarea
                 else
                    ! if no PFT area in primary lands, set bare ground fraction to one.
                    if ( i_landusetype .eq. primaryland) then
                       sites(s)%area_bareground = 1._r8
+                      sites(s)%area_pft(:, i_landusetype) = 0._r8
                    endif
                 end if
              end do
