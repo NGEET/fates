fc3e66c22d6637b5f56a09d7dfea08825c7acc63
adrifoster <adrifost@ucar.edu>
2024-04-16 15:30:12 -0600

    get plotting to work

--- functional_unit_testing/allometry/FatesTestAllometry.F90
+++ functional_unit_testing/allometry/FatesTestAllometry.F90
@@ -10,22 +10,133 @@ program FatesTestAllometry
   ! LOCALS:
   type(fates_unit_test_param_reader) :: param_reader
   character(len=*), parameter        :: param_file = 'fates_params_default.nc'
+  character(len=*), parameter        :: out_file = 'allometry_out.nc'
   integer                            :: numpft
-  integer                            :: i
-  real(r8)                           :: height
+  integer                            :: i, j
+  integer                            :: numdbh
+  real(r8), allocatable              :: dbh(:)       ! diameter at breast height [cm]
+  real(r8), allocatable              :: height(:, :) ! height [m]
 
   ! CONSTANTS:
   real(r8) :: min_dbh = 0.5_r8   ! minimum DBH to calculate [cm]
   real(r8) :: max_dbh = 200.0_r8 ! maximum DBH to calculate [cm]
+  real(r8) :: dbh_inc = 0.5_r8   ! DBH increment to use [cm]
+
+  interface
+
+    subroutine WriteAllometryData(out_file, ndbh, numpft, dbh, height)
+
+      use FatesUnitTestIOMod, only : OpenNCFile, RegisterNCDims, CloseNCFile
+      use FatesUnitTestIOMod, only : RegisterVar1D, WriteVar, RegisterVar2D
+      use FatesUnitTestIOMod, only : type_double, type_int
+      use FatesConstantsMod,  only : r8 => fates_r8
+      implicit none
+
+      character(len=*), intent(in) :: out_file
+      integer,          intent(in) :: ndbh, numpft
+      real(r8),         intent(in) :: dbh(:)
+      real(r8),         intent(in) :: height(:,:)
+
+    end subroutine WriteAllometryData
+
+  end interface
   
   ! read in parameter file
   call param_reader%Init(param_file)
   call param_reader%RetrieveParameters()
-  numpft = size(prt_params%wood_density, dim=1) - 1
+  numpft = size(prt_params%wood_density, dim=1)
 
-  do i = 1, numpft
-    call h_allom(25.0_r8, i, height)
-    print *, height
+  ! allocate arrays and initialize DBH array
+  numdbh = int((max_dbh - min_dbh)/dbh_inc + 1)
+  
+  allocate(dbh(numdbh))
+  allocate(height(numdbh, numpft))
+  
+  do i = 1, numdbh
+    dbh(i) = min_dbh + dbh_inc*(i-1)
   end do
+
+  ! calculate allometries
+  do i = 1, numpft
+    do j = 1, numdbh
+      call h_allom(dbh(j), i, height(j, i))
+    end do
+  end do
+
+  call WriteAllometryData(out_file, numdbh, numpft, dbh, height)
   
 end program FatesTestAllometry
+
+! ----------------------------------------------------------------------------------------
+
+subroutine WriteAllometryData(out_file, numdbh, numpft, dbh, height)
+  !
+  ! DESCRIPTION:
+  ! Writes out data from the allometry test
+  !
+  use FatesConstantsMod,  only : r8 => fates_r8
+  use FatesUnitTestIOMod, only : OpenNCFile, RegisterNCDims, CloseNCFile
+  use FatesUnitTestIOMod, only : RegisterVar1D, WriteVar, RegisterVar2D
+  use FatesUnitTestIOMod, only : EndNCDef
+  use FatesUnitTestIOMod, only : type_double, type_int
+
+  implicit none
+
+  ! ARGUMENTS:
+  character(len=*), intent(in) :: out_file
+  integer,          intent(in) :: numdbh, numpft
+  real(r8),         intent(in) :: dbh(:)
+  real(r8),         intent(in) :: height(:,:)
+
+  ! LOCALS:
+  integer, allocatable :: pft_indices(:) ! array of pft indices to write out
+  integer              :: i              ! looping index
+  integer              :: ncid           ! netcdf file id
+  character(len=8)     :: dim_names(2)   ! dimension names
+  integer              :: dimIDs(2)      ! dimension IDs
+  integer              :: dbhID, pftID   ! variable IDs for dimensions
+  integer              :: heightID
+
+  ! create pft indices
+  allocate(pft_indices(numpft))
+  do i = 1, numpft
+    pft_indices(i) = i
+  end do
+
+  ! dimension names
+  dim_names = [character(len=12) :: 'dbh', 'pft']
+
+  ! open file
+  call OpenNCFile(trim(out_file), ncid, 'readwrite')
+
+  ! register dimensions
+  call RegisterNCDims(ncid, dim_names, (/numdbh, numpft/), 2, dimIDs)
+
+  ! register dbh
+  call RegisterVar1D(ncid, dim_names(1), dimIDs(1), type_double,           &
+    [character(len=20)  :: 'units', 'long_name'],                        &
+    [character(len=150) :: 'cm', 'diameter at breast height'], 2, dbhID)
+
+  ! register pft
+  call RegisterVar1D(ncid, dim_names(2), dimIDs(2), type_int,           &
+    [character(len=20)  :: 'units', 'long_name'],                     &
+    [character(len=150) :: '', 'plant functional type'], 2, pftID)
+
+  ! register height
+  call RegisterVar2D(ncid, 'height', dimIDs(1:2), type_double,     &
+    [character(len=20)  :: 'coordinates', 'units', 'long_name'], &
+    [character(len=150) :: 'pft dbh', 'm', 'plant height'],      &                                                  
+    3, heightID)
+
+  ! finish defining variables
+  call EndNCDef(ncid)
+
+  ! write out data
+  call WriteVar(ncid, dbhID, dbh(:))
+  call WriteVar(ncid, pftID, pft_indices(:))
+  call WriteVar(ncid, heightID, height(:,:))
+
+  ! close the file
+  call CloseNCFile(ncid)
+
+end subroutine WriteAllometryData
--- functional_unit_testing/run_fates_tests.py
+++ functional_unit_testing/run_fates_tests.py
@@ -1,23 +1,97 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import os
 import sys
 from build_fortran_tests import build_unit_tests
 
-_FATES_PYTHON = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(1, _FATES_PYTHON)
+import math
+import pandas as pd
+import numpy as np
+import xarray as xr
+import matplotlib
+import matplotlib.pyplot as plt
 
 from utils import add_cime_lib_to_path
 add_cime_lib_to_path()
 
 from CIME.utils import run_cmd_no_fail
 
+def round_up(n, decimals=0):
+    multiplier = 10**decimals
+    return math.ceil(n * multiplier) / multiplier
+  
+def truncate(n, decimals=0):
+    multiplier = 10**decimals
+    return int(n * multiplier) / multiplier
+  
+def get_color_pallete():
+    
+    colors = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
+            (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
+            (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
+            (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
+            (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+    
+    for i in range(len(colors)):
+        r, g, b = colors[i]
+        colors[i] = (r/255., g/255., b/255.)
+        
+    return colors
+
+
+def plot_allometry_var(data, var, varname, units):
+    df = pd.DataFrame({'dbh': np.tile(data.dbh, len(data.pft)),
+                       'pft': np.repeat(data.pft, len(data.dbh)),
+                       var: data.values.flatten()})
+    
+    maxdbh = df['dbh'].max()
+    maxvar = round_up(df[var].max())
+    
+    colors = get_color_pallete()
+    
+    plt.figure(figsize=(7, 5))
+    ax = plt.subplot(111)
+    ax.spines["top"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+
+    plt.xlim(0.0, maxdbh)
+    plt.ylim(0.0, maxvar)
+
+    plt.yticks(fontsize=10)
+    plt.xticks(fontsize=10)
+    
+    for y in range(0, int(maxvar), 5):
+        plt.plot(range(math.floor(0), math.ceil(maxdbh)),
+                  [y] * len(range(math.floor(0), math.ceil(maxdbh))),
+                  "--", lw=0.5, color="black", alpha=0.3)
+        
+    plt.tick_params(bottom=False, top=False, left=False, right=False)
+    
+    pfts = np.unique(df.pft.values)
+    for rank, pft in enumerate(pfts):
+        data = df[df.pft == pft]
+        plt.plot(data.dbh.values, data[var].values, lw=2, color=colors[rank],
+                 label=pft)
+        
+    plt.xlabel('DBH (cm)', fontsize=11)
+    plt.ylabel(f'{varname} ({units})', fontsize=11)
+    plt.title(f"Simulated {varname} for input parameter file", fontsize=11)
+    plt.legend(loc='upper left', title='PFT')
+    
+    plt.show()
+  
 
 if __name__ == "__main__":
     
     ## Arguments
     clean = True
-    build = True
+    build = False
+    run = False
     build_dir = "../_build"
     name = "fates_unit_tests"
     make_j = 8
@@ -26,24 +100,36 @@ if __name__ == "__main__":
     param_cdl_path = "../parameter_files/fates_params_default.cdl"
     
     ## Constants
+    out_file = "allometry_out.nc"
     test_dir = "fates_allom_test"
     test_exe = "FATES_allom_exe"
     
+    build_dir_path = os.path.abspath(build_dir)    
+    
+    ## Actual Program
     if build:
         build_unit_tests(build_dir, name, cmake_directory, make_j, clean=clean)
     
-    build_dir_path = os.path.abspath(build_dir)    
-    exe_path = os.path.join(build_dir_path, test_dir, test_exe)
-    
-    if param_file is None:
-      file_basename = os.path.basename(param_cdl_path).split(".")[-2]
-      file_nc_name = f"{file_basename}.nc"
-      file_gen_command = [
-        "ncgen -o",
-        os.path.join(file_nc_name),
-        param_cdl_path
-      ]
-      run_cmd_no_fail(" ".join(file_gen_command), combine_output=True)
+    if run:
+        exe_path = os.path.join(build_dir_path, test_dir, test_exe)
+        
+        if param_file is None:
+          file_basename = os.path.basename(param_cdl_path).split(".")[-2]
+          file_nc_name = f"{file_basename}.nc"
+          file_gen_command = [
+            "ncgen -o",
+            os.path.join(file_nc_name),
+            param_cdl_path
+          ]
+          run_cmd_no_fail(" ".join(file_gen_command), combine_output=True)
           
-    out = run_cmd_no_fail(exe_path, combine_output=True)
-    print(out)
+          out = run_cmd_no_fail(exe_path, combine_output=True)
+          print(out)
+    
+    # read in allometry data
+    allometry_dat = xr.open_dataset(os.path.join(build_dir_path, out_file))
+    
+    # plot allometry data
+    plot_allometry_var(allometry_dat.height, 'height', 'height', 'm')
+    
+    
