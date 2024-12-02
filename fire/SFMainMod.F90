f7ed823ce551c3aa6c19f5cbb253d0f63f962071
adrifoster <adrifost@ucar.edu>
2024-12-02 12:16:03 -0700

    create temp variable

--- fire/SFMainMod.F90
+++ fire/SFMainMod.F90
@@ -228,6 +228,7 @@ contains
     real(r8) beta_ratio           ! ratio of beta/beta_op
     real(r8) a_beta               ! dummy variable for product of a* beta_ratio for react_v_opt equation
     real(r8) a,b,c,e              ! function of fuel sav
+    real(r8) non_mineral_loading  ! non-mineral loading [kgC/m2]
 
     logical, parameter :: debug_windspeed = .false. !for debugging
     real(r8),parameter :: q_dry = 581.0_r8          !heat of pre-ignition of dry fuels (kJ/kg) 
@@ -239,7 +240,7 @@ contains
       if(currentPatch%nocomp_pft_label .ne. nocomp_bareground .and. currentPatch%fuel%non_trunk_loading > nearzero) then
                        
        ! remove mineral content from net fuel load per Thonicke 2010 for ir calculation
-       currentPatch%fuel%non_trunk_loading = currentPatch%fuel%non_trunk_loading * (1.0_r8 - SF_val_miner_total) !net of minerals
+       non_mineral_loading = currentPatch%fuel%non_trunk_loading*(1.0_r8 - SF_val_miner_total) !net of minerals
 
        ! ----start spreading---
 
@@ -330,8 +331,8 @@ contains
             (3.52_r8*(mw_weight**3.0_r8))))
 
        ! ir = reaction intenisty in kJ/m2/min
-       ! currentPatch%fuel%non_trunk_loading converted from kgC/m2 to kgBiomass/m2 for ir calculation
-       ir = reaction_v_opt*(currentPatch%fuel%non_trunk_loading/0.45_r8)*SF_val_fuel_energy*moist_damp*SF_val_miner_damp 
+       ! non_mineral_loading converted from kgC/m2 to kgBiomass/m2 for ir calculation
+       ir = reaction_v_opt*(non_mineral_loading/0.45_r8)*SF_val_fuel_energy*moist_damp*SF_val_miner_damp 
 
        ! write(fates_log(),*) 'ir',gamma_aptr,moist_damp,SF_val_fuel_energy,SF_val_miner_damp
 
@@ -368,6 +369,7 @@ contains
     real(r8) :: moist           !effective fuel moisture
     real(r8) :: tau_b(num_fuel_classes)     !lethal heating rates for each fuel class (min) 
     real(r8) :: fc_ground(num_fuel_classes) !total amount of fuel consumed per area of burned ground (kg C / m2 of burned area)
+    real(r8) :: non_mineral_loading ! non-mineral loading [kgC/m2]
     integer :: tr_sf, tw_sf, dl_sf, lg_sf
     integer  :: c
     
@@ -422,8 +424,6 @@ contains
          FC_ground(dl_sf)       = currentPatch%fuel%frac_burnt(dl_sf)   * sum(litt_c%leaf_fines(:))
          FC_ground(lg_sf)       = currentPatch%fuel%frac_burnt(lg_sf)   * currentPatch%livegrass  
          
-         !call currentPatch%fuel%BurnFuel(fc_ground)
-
        ! Following used for determination of cambial kill follows from Peterson & Ryan (1986) scheme 
        ! less empirical cf current scheme used in SPITFIRE which attempts to mesh Rothermel 
        ! and P&R, and while solving potential inconsistencies, actually results in BIG values for 
@@ -431,8 +431,9 @@ contains
        ! taul is the duration of the lethal heating.  
        ! The /10 is to convert from kgC/m2 into gC/cm2, as in the Peterson and Ryan paper #Rosie,Jun 2013
         
+       non_mineral_loading = currentPatch%fuel%non_trunk_loading*(1.0_r8 - SF_val_miner_total) !net of minerals
        do c = 1,num_fuel_classes 
-          tau_b(c)   =  39.4_r8 *(currentPatch%fuel%frac_loading(c)*currentPatch%fuel%non_trunk_loading/0.45_r8/10._r8)* &
+          tau_b(c)   =  39.4_r8 *(currentPatch%fuel%frac_loading(c)*non_mineral_loading/0.45_r8/10._r8)* &
                (1.0_r8-((1.0_r8-currentPatch%fuel%frac_burnt(c))**0.5_r8))  
        enddo
        tau_b(tr_sf)   =  0.0_r8
