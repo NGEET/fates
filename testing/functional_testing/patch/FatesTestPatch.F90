c173889db8b15fb28a92d72e84590ca0cb774696
adrifoster <adrifost@ucar.edu>
2025-01-14 12:08:32 -0700

    add new insertcohort

--- testing/functional_testing/patch/FatesTestPatch.F90
+++ testing/functional_testing/patch/FatesTestPatch.F90
@@ -17,78 +17,71 @@ program FatesTestPatch
   type(fates_patch_type),            pointer     :: patch        ! patch
   type(fates_cohort_type),           pointer     :: cohort       ! cohort
   integer                                        :: i            ! patch array location
-  type(fates_cohort_type),               pointer :: head, new_node
-  real(r8) :: heights(8) = (/10.0_r8, 100.0_r8, 15.0_r8, 2.0_r8, 1.0_r8, 12.5001_r8, 20.0_r8, 0.5_r8/)
-  
-     ! create a hardcoded doubly linked list
-      allocate(patch)
-      
-      allocate(head)
-    
-      head%height = heights(1)
-      patch%shortest => head
-    
-      allocate(new_node)
-      new_node%height = heights(2)
-      head%taller => new_node
-      
-      allocate(new_node)
-      new_node%height = heights(3)
-      head%taller%taller => new_node
-      new_node%shorter => head%taller
-      
-      allocate(new_node)
-      new_node%height = heights(4)
-      head%taller%taller%taller => new_node
-      new_node%shorter => head%taller%taller
-      
-      allocate(new_node)
-      new_node%height = heights(5)
-      head%taller%taller%taller%taller => new_node
-      new_node%shorter => head%taller%taller%taller
-      
-      allocate(new_node)
-      new_node%height = heights(6)
-      head%taller%taller%taller%taller%taller => new_node
-      new_node%shorter => head%taller%taller%taller%taller
-      
-      allocate(new_node)
-      new_node%height = heights(7)
-      head%taller%taller%taller%taller%taller%taller => new_node
-      new_node%shorter => head%taller%taller%taller%taller%taller
-      
-      allocate(new_node)
-      new_node%height = heights(8)
-      head%taller%taller%taller%taller%taller%taller%taller => new_node
-      new_node%shorter => head%taller%taller%taller%taller%taller%taller
-    
-      patch%tallest => new_node
-      
-      ! sort cohorts
-      call patch%SortCohorts()
-      
-      ! check backwards and forwards
-      new_node => patch%shortest
-      do while (associated(new_node))
-        if (associated(new_node%taller)) then
-          print *, new_node%height
-        end if
-        new_node => new_node%taller
-      end do
-      
-      new_node => patch%tallest
-      do while (associated(new_node))
-        if (associated(new_node%shorter)) then 
-        print *, new_node%height
-        end if
-        new_node => new_node%shorter
-      end do
+  type(fates_cohort_type), pointer               :: storebigcohort
+  type(fates_cohort_type), pointer               :: storesmallcohort
+  integer                                        :: tnull, snull
 
-  ! ! CONSTANTS:
-  ! integer,  parameter :: num_levsoil = 10      ! number of soil layers
-  ! real(r8), parameter :: step_size = 1800.0_r8 ! step-size [s]
+  ! CONSTANTS:
+  integer,  parameter :: num_levsoil = 10      ! number of soil layers
+  real(r8), parameter :: step_size = 1800.0_r8 ! step-size [s]
+  integer,  parameter :: num_cohorts = 2
+  real(r8), parameter :: height = 35.0
   
-  ! ! read in parameter file name from command line
+  ! insert 100 cohorts of the same height
+  allocate(patch)
+  
+  do i = 1, num_cohorts
+    
+    allocate(cohort)
+    cohort%height = height
+    cohort%dbh = i
+    
+    storebigcohort => patch%tallest
+    storesmallcohort => patch%shortest
+  
+    if(associated(patch%tallest)) then
+        tnull = 0
+      else
+        tnull = 1
+        patch%tallest => cohort
+        cohort%taller => null()
+      endif
+
+      if(associated(patch%shortest))then
+        snull = 0
+      else
+        snull = 1
+        patch%shortest => cohort
+        cohort%shorter => null()
+      endif 
+      
+      call patch%InsertCohort(cohort)
+      !call patch%InsertCohort(cohort, patch%tallest,           &
+      !  patch%shortest, tnull, snull, storebigcohort, storesmallcohort)
+      
+      !patch%tallest  => storebigcohort
+      !patch%shortest => storesmallcohort
+  end do
+  
+  cohort => patch%shortest
+  do while (associated(cohort))
+    print *, cohort%dbh
+    cohort => cohort%taller
+  end do
+  
+  print *, '------------------'
+  
+  call patch%SortCohorts_new()
+  
+  print *, '------------------'
+  
+  cohort => patch%shortest
+  do while (associated(cohort))
+    print *, cohort%dbh
+    cohort => cohort%taller
+  end do
+
+  ! read in parameter file name from command line
   ! param_file = command_line_arg(1)
   
   ! ! read in parameter file
@@ -113,32 +106,5 @@ program FatesTestPatch
   ! end do
   ! write(*,*) ' '
 
-  ! ! randomize the heights
-  ! i = 1
-  ! cohort => patch%shortest
-  ! do while (associated(cohort))
-  !   cohort%height = rand_heights(i)
-  !   cohort => cohort%taller
-  !   i = i + 1
-  ! end do 
-  ! cohort => patch%shortest
-  ! write(*,*) 'Now they should be out of order: '
-  ! do while (associated(cohort))
-  !   write (*,*) cohort%height, cohort%dbh
-  !   cohort => cohort%taller
-  ! end do
-  ! write(*,*) ' '
-  
-  ! call patch%SortCohorts2()
-  
-  !   ! print out list in ascending order
-  ! cohort => patch%shortest
-  ! write(*,*) 'Back in order?'
-  ! do while (associated(cohort))
-  !   write (*,*) cohort%height, cohort%dbh
-  !   cohort => cohort%taller
-  ! end do
-  ! write(*,*) ' '
-  
 end program FatesTestPatch
 
