b1ccc18d70c5747344299334f34707b4d5603c80
Ryan Knox <rgknox@lbl.gov>
2025-02-02 16:35:45 -0700

    added nocomp filter on history logic

--- main/FatesHistoryInterfaceMod.F90
+++ main/FatesHistoryInterfaceMod.F90
@@ -15,6 +15,7 @@ module FatesHistoryInterfaceMod
   use FatesConstantsMod        , only : i_term_mort_type_canlev
   use FatesConstantsMod        , only : i_term_mort_type_numdens
   use FatesConstantsMod        , only : nocomp_bareground_land
+  use FatesConstantsMod        , only : nocomp_bareground
   use FatesGlobals             , only : fates_log
   use FatesGlobals             , only : endrun => fates_endrun
   use EDParamsMod              , only : nclmax, maxpft
@@ -5071,11 +5072,11 @@ contains
                     cpatch%c_lblayer * cpatch%total_canopy_area * mol_per_umol * site_area_veg_inv
 
                ! Only accumulate the instantaneous vegetation temperature for vegetated patches
-               !if (cpatch%nocomp_pft_label.ne.nocomp_bareground)then
+               if (cpatch%nocomp_pft_label.ne.nocomp_bareground)then
                   hio_tveg(io_si) = hio_tveg(io_si) + &
                        (bc_in(s)%t_veg_pa(cpatch%patchno) - t_water_freeze_k_1atm) * &
                        cpatch%total_canopy_area * site_area_veg_inv
-               !end if
+               end if
 
                ccohort => cpatch%shortest
                do while(associated(ccohort))
