3ae48377d5222035f217a1c31505d6ef47276c74
Marcos Longo <mdplongo@gmail.com>
2025-03-10 10:22:59 -0700

    Fix another bug in the if/select case transition.

--- biogeochem/EDPhysiologyMod.F90
+++ biogeochem/EDPhysiologyMod.F90
@@ -1599,7 +1599,7 @@ contains
              ! This PFT is not deciduous.
              is_flushing_time         = .false.
              is_shedding_time         = .false.
-          end if is_time_block
+          end select is_time_block
 
 
 
