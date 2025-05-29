e43e22f3b4f91e647c77a862e57bcec4b5379dbf
Gregory Lemieux <glemieux@users.noreply.github.com>
2025-05-29 15:24:43 -0700

    Revert "remove unused bc_in from canopy_summarization"
    This reverts commit 2ee65cd5706bfd6ed2924dec27253d7fe048f887.

--- biogeochem/EDCanopyStructureMod.F90
+++ biogeochem/EDCanopyStructureMod.F90
@@ -1307,7 +1307,7 @@ contains
 
   ! =====================================================================================
 
-  subroutine canopy_summarization( nsites, sites )
+  subroutine canopy_summarization( nsites, sites, bc_in )
 
     ! ----------------------------------------------------------------------------------
     ! Much of this routine was once ed_clm_link minus all the IO and history stuff
@@ -1323,6 +1323,7 @@ contains
     ! !ARGUMENTS
     integer                 , intent(in)            :: nsites
     type(ed_site_type)      , intent(inout), target :: sites(nsites)
+    type(bc_in_type)        , intent(in)            :: bc_in(nsites)
     !
     ! !LOCAL VARIABLES:
     type (fates_patch_type)  , pointer :: currentPatch
