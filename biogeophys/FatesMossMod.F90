de247a898b4319d7a114bd1347ab767b0d9b3638
Sam Rabin <sam.rabin@gmail.com>
2025-06-11 11:17:00 -0600

    Moss: Rename prod to moss_biom_change.

--- biogeophys/FatesMossMod.F90
+++ biogeophys/FatesMossMod.F90
@@ -72,7 +72,7 @@ subroutine moss(alff, cla_m2_per_plot, decLit_t_per_haplot, moss_biom_kg_per_plo
   real(r8) :: assim_eff_kg_per_m2plot ! Assimilation (kg/m2)
   real(r8) :: repro_eff_kg_per_kgmoss ! Effective reproduction (kg/kg)
   real(r8) :: repro_eff_kg_per_m2plot ! Effective reproduction (kg/m2)
-  real(r8) :: prod_kg_per_m2plot      ! Moss production (kg/m2)
+  real(r8) :: moss_biom_change_kg_per_m2plot     ! Change in moss biomass (kg/m2) during this timestep
   real(r8) :: moss_to_litter_flux_kg_per_m2plot  ! Flux from moss to litter (kg/m2)
   real(r8) :: moss_to_litter_flux_kg_per_plot    ! Flux from moss to litter (kg)
 
@@ -120,21 +120,23 @@ subroutine moss(alff, cla_m2_per_plot, decLit_t_per_haplot, moss_biom_kg_per_plo
   ! Effective assimilation
   assim_eff_kg_per_kgmoss = SLA_M2LEAF_PER_KGMOSS*assim_kg_per_m2leaf*(1.0 - repro_eff_kg_per_kgmoss)
   assim_eff_kg_per_m2plot = assim_eff_kg_per_kgmoss * moss_biom_kg_per_m2plot_before + repro_eff_kg_per_m2plot
-  prod_kg_per_m2plot = assim_eff_kg_per_m2plot - moss_biom_kg_per_m2plot_before*(Q_KG_PER_KGMOSS + B_KG_PER_KGMOSS)
 
-  if (moss_biom_kg_per_m2plot_before + prod_kg_per_m2plot < 0.0) then
+  ! Net change in moss biomass
+  moss_biom_change_kg_per_m2plot = assim_eff_kg_per_m2plot - moss_biom_kg_per_m2plot_before*(Q_KG_PER_KGMOSS + B_KG_PER_KGMOSS)
+
+  if (moss_biom_kg_per_m2plot_before + moss_biom_change_kg_per_m2plot < 0.0) then
       ! Not enough moss to account for mortality/respiration
       ! Set moss loss to all of current biomass
-      prod_kg_per_m2plot = -1.0*moss_biom_kg_per_m2plot_before
+      moss_biom_change_kg_per_m2plot = -1.0 * moss_biom_kg_per_m2plot_before
   end if
 
   ! Calculate litter flux (kg)
   ! TODO: Flux to litter should only come from mortality. Respiration should go to atmosphere.
-  moss_to_litter_flux_kg_per_m2plot = assim_eff_kg_per_m2plot - prod_kg_per_m2plot
+  moss_to_litter_flux_kg_per_m2plot = assim_eff_kg_per_m2plot - moss_biom_change_kg_per_m2plot
   moss_to_litter_flux_kg_per_m2plot = max(0.0, moss_to_litter_flux_kg_per_m2plot)
 
   ! Update biomass
-  moss_biom_kg_per_m2plot_after = moss_biom_kg_per_m2plot_before + prod_kg_per_m2plot
+  moss_biom_kg_per_m2plot_after = moss_biom_kg_per_m2plot_before + moss_biom_change_kg_per_m2plot
 
   ! Thickness of live moss layer (m)
   livemoss_depth_m = moss_biom_kg_per_m2plot_after / BULK_MOSS_KG_PER_M3
