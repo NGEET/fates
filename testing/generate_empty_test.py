49478f8f8d736415ef6ac498df3ddc72a60ddd33
adrifoster <adrifost@ucar.edu>
2025-06-20 09:56:41 -0600

    some updates

--- testing/generate_empty_test.py
+++ testing/generate_empty_test.py
@@ -18,11 +18,11 @@ Specifically, this script peforms the following steps:
 
 """
 
-import os 
+import os
 import argparse
 import textwrap
-from utils import snake_to_camel
-from test_class import generate_test
+from test_generator_class import generate_test
+
 
 def commandline_args():
     """Parse and return command-line arguments"""
@@ -33,18 +33,24 @@ def commandline_args():
     subparsers = parser.add_subparsers(
         help="Two types of tests, either:", dest="test_type"
     )
-    unit_parser = subparsers.add_parser('unit', help="Create an empty unit test")
-    functional_parser = subparsers.add_parser('functional', help="Create an empty functional test")
-    
+    unit_parser = subparsers.add_parser("unit", help="Create an empty unit test")
+    functional_parser = subparsers.add_parser(
+        "functional", help="Create an empty functional test"
+    )
+
     # Unit test parser options
-    
+
     # Functional test parser options
-    
+
     # common options between both subparsers
     for subparser in [unit_parser, functional_parser]:
-        subparser.add_argument('--test-name', default="hello_world", help="Name of the new test")
-        subparser.add_argument('--test-sub-dir', default=None, help='Optional test subdirectory path')
-        
+        subparser.add_argument(
+            "--test-name", default="hello_world", help="Name of the new test"
+        )
+        subparser.add_argument(
+            "--test-sub-dir", default=None, help="Optional test subdirectory path"
+        )
+
     # print help for both subparsers
     parser.epilog = textwrap.dedent(
         f"""\
@@ -52,54 +58,44 @@ def commandline_args():
          {functional_parser.format_help()}
          """
     )
-    
+
     args = parser.parse_args()
-    
+
     return args
 
-def get_config_path(test_type: str) -> str:
-    """Returns path to config file based on input test type
 
-    Args:
-        test_type (str): test type [unit, functional]
+# def get_config_path(test_type: str) -> str:
+#     """Returns path to config file based on input test type
 
-    Raises:
-        RuntimeError: test_type must be either unit or functional
+#     Args:
+#         test_type (str): test type [unit, functional]
 
-    Returns:
-        str: path to config file
-    """
-    if test_type == 'unit':
-        return _UNIT_TESTS_CONFIG
-    elif test_type == 'functional':
-        return _FUNCTIONAL_TESTS_CONFIG
-    else:
-        raise RuntimeError("test_type must be one of [unit, functional]")
-        
-def update_config(config_path: str, test_name: str, build_dir: str):
-    if not os.path.exists(config_path):
-        raise FileNotFoundError(f"Warning: {config_path} not found.")
-    
-    if os.path.exists(config_path):
-        with open(config_path, "a") as f:
-            f.write(f"[{test_name}]\n")
-            f.write(f"test_dir = {build_dir}\n")
-        print(f"Updated {config_path} with {test_name}")
+#     Raises:
+#         RuntimeError: test_type must be either unit or functional
 
-        
+#     Returns:
+#         str: path to config file
+#     """
+#     if test_type == 'unit':
+#         return _UNIT_TESTS_CONFIG
+#     elif test_type == 'functional':
+#         return _FUNCTIONAL_TESTS_CONFIG
+#     else:
+#         raise RuntimeError("test_type must be one of [unit, functional]")
+
 
 def main():
-  
-    #args = commandline_args()
+
+    # args = commandline_args()
 
     # Example - create a unit test
     unit_test = generate_test("unit", "hello_world")
     unit_test.setup_test()
 
     # Example: Creating a functional test
-    functional_test = generate_test("functional", "hello_world")
-    functional_test.setup_test()
+    # functional_test = generate_test("functional", "hello_world")
+    # functional_test.setup_test()
+
 
 if __name__ == "__main__":
-  main()
-  
+    main()
