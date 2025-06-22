620abe93ec0247bd2f4c483db82a640439c0c09c
Sam Rabin <sam.rabin@gmail.com>
2025-06-22 11:44:50 -0600

    FunctionalClass: Revert unused changes.

--- testing/functional_class.py
+++ testing/functional_class.py
@@ -1,5 +1,3 @@
-import os
-import urllib.request
 from abc import ABC, abstractmethod
 from utils import str_to_bool, str_to_list
 
@@ -15,7 +13,7 @@ class FunctionalTest(ABC):
         self.use_param_file = str_to_bool(use_param_file)
         self.other_args = str_to_list(other_args)
         self.plot = False
-
+        
     @abstractmethod
     def plot_output(self, run_dir:str, save_figs:bool, plot_dir:str):
         pass
