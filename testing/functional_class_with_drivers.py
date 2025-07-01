8caf366b48d4aa8caee02b1930810f7c2607c3a0
Sam Rabin <sam.rabin@gmail.com>
2025-07-01 10:47:57 -0600

    Remove Python download of datm_file.

--- testing/functional_class_with_drivers.py
+++ testing/functional_class_with_drivers.py
@@ -1,5 +1,4 @@
 import os
-import urllib.request
 from functional_class import FunctionalTest
 
 
@@ -11,12 +10,6 @@ class FunctionalTestWithDrivers(FunctionalTest):
         # Check that datm exists and save its absolute path
         self.datm_file = os.path.abspath(datm_file)
         if not os.path.exists(self.datm_file):
-            if not datm_file_url:
-                raise FileNotFoundError(f"datm_file not found: '{self.datm_file}'")
-            datm_file_dir = os.path.dirname(self.datm_file)
-            if not os.path.isdir(datm_file_dir):
-                os.makedirs(datm_file_dir)
-            print(f"Downloading datm_file from {datm_file_url}")
-            urllib.request.urlretrieve(datm_file_url, self.datm_file)
+            raise FileNotFoundError(f"datm_file not found: '{self.datm_file}'")
 
         super().__init__(*args)
