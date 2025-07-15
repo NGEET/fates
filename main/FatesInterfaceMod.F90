62794b28cac9c8f34196ad3c83e58bb9f141919c
Gregory Lemieux <glemieux@users.noreply.github.com>
2025-07-15 15:35:25 -0700

    correct do while statement

--- main/FatesInterfaceMod.F90
+++ main/FatesInterfaceMod.F90
@@ -2720,7 +2720,7 @@ subroutine FatesReadParameters(param_reader)
 
    currentPatch => this%oldest_patch
 
-   do while associated(currentPatch)
+   do while (associated(currentPatch))
 
       p = this%patch_map(currentPatch%patchno)
 
@@ -2756,7 +2756,7 @@ subroutine FatesReadParameters(param_reader)
 
    currentPatch => this%oldest_patch
 
-   do while associated(currentPatch)
+   do while (associated(currentPatch))
 
       c = this%column_map(currentPatch%patchno)
 
