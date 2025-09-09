d950a6d9cfda4fe891d66a14529c63ad0d23f8bd
Gregory Lemieux <glemieux@users.noreply.github.com>
2025-09-09 14:36:21 -0700

    correct end function name

--- main/FatesInterfaceTypesMod.F90
+++ main/FatesInterfaceTypesMod.F90
@@ -1019,7 +1019,7 @@ module FatesInterfaceTypesMod
 
     key = this%vars(index)%key
 
-  end function GetRegistryIndex
+  end function GetRegistryKey
 
   ! ======================================================================================
   
