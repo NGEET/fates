f3c34e71e9b870a40d2132ce967c9943790e2feb
Gregory Lemieux <glemieux@users.noreply.github.com>
2025-09-11 11:57:11 -0700

    add case select logic and pointers to index into the source data pointer

--- main/FatesInterfaceVarTypeMod.F90
+++ main/FatesInterfaceVarTypeMod.F90
@@ -100,34 +100,52 @@ module FatesInterfaceVariableTypeMod
     
     subroutine UpdateInterfaceVariable(this, var, subgrid_indices)
       
-      class(fates_interface_variable_type) :: this
+      class(fates_interface_variable_type) :: this             ! variable being updated
+      class(fates_interface_variable_type), intent(in) :: var  ! variable update source
+      integer, intent(in) :: subgrid_indices(:)                ! subgrid indices for the update source
 
-      class(fates_interface_variable_type), intent(in) :: var
-      integer, intent(in) :: subgrid_indices(:)
+      class(*), pointer :: data_var0d        => null()
+      class(*), pointer :: data_var1d(:)     => null()
+      class(*), pointer :: data_var2d(:,:)   => null()
+      class(*), pointer :: data_var3d(:,:,:) => null()
+
+      integer :: index  ! index for the subgrid level of the input interface variable 
 
       ! This update method assumes that the first rank of the HLM data arrays
       ! corresponds to the subgrid level of the interface variable type.
       ! E.g. col_cf%w_scalar(c,1:nlevsoil) shows that the first rank is the column index.
       ! TODO: This should be held in an interface requirements document.
-      
 
+      ! Get the subgrid index for the updating variable 
+      index = subgrid_indices(var%subgrid_index)
 
-      ! TODO: add column index to the interface variable type to allow
-      ! for appropriate slicing of input pointer array
-      ! e.g.
-      ! if (this%rank == 1)) then
-      !   data_this => this%data1d
-      ! else if (this%rank == 2) then
-      !   data_this => this%data2d
-      ! end if
-      ! if (var%rank == 1)) then
-      !   data_var => var%data1d
-      ! else if (this%rank == 2) then
-      !   data_var => var%data2d(this%col,:)
-      ! end if
-      ! data_this = data_var()
-      ! This isn't exactly right, but you get the idea
+      ! Update the data pointer based on the rank of the source variable while indexing
+      ! into the appropriate subgrid level
+      ! TODO: This assumes HLM->FATES direction; Validate this for FATES->HLM direction
+      select case (var%rank)
+        case(0)
+          data_var0d => var%data0d
+        case(1)
+          data_var0d => var%data1d(index)
+        case(2)
+          data_var1d => var%data2d(index,:)
+        case(3)
+          data_var2d => var%data3d(index,:,:)
+        case default
+          call endrun(fates_log, 'FATES ERROR: Unsupported interface variable source rank in UpdateInterfaceVariable')
+      end select
 
+      ! Update the data pointer of the target variable based on its rank
+      select case (this%rank)
+        case(0)
+          this%data0d = data_var0d
+        case(1)
+          this%data1d = data_var1d
+        case(2)
+          this%data2d = data_var2d
+        case default
+          call endrun(fates_log, 'FATES ERROR: Unsupported interface variable input rank in UpdateInterfaceVariable')
+      end select
 
     end subroutine UpdateInterfaceVariable
 
