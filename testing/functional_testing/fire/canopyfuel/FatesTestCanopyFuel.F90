f1c9eb97bc649c59a956876c49a8e9705b0e1e9a
Xiulin Gao <xiulin.gao.edu@gmail.com>
2025-10-02 11:24:59 -0600

    change test patch

--- testing/functional_testing/fire/canopyfuel/FatesTestCanopyFuel.F90
+++ testing/functional_testing/fire/canopyfuel/FatesTestCanopyFuel.F90
@@ -100,7 +100,7 @@ program FatesTestCanopyFuel
   
   ! fuel models and patch types to test
   integer, parameter, dimension(2) :: fuel_models = (/10, 11/)
-  integer, parameter, dimension(4) :: patch_ids = (/1, 6, 7, 8/)
+  integer, parameter, dimension(3) :: patch_ids = (/1, 6, 7/)
   
   ! number of fuel models  and patch types to test
   num_fuel_models = size(fuel_models)
@@ -251,6 +251,13 @@ program FatesTestCanopyFuel
             tfc_ros = sum(fuel_consumed) - fuel_consumed(fuel_classes%trunks())
             FI(p,f) = FireIntensity(tfc_ros/0.45_r8, ROS_front(p,f)/60.0_r8)
 
+            write(*,*) 'CBD is', fuel(f)%canopy_bulk_density
+            write(*,*) 'wind is', wind
+            write(*,*) 'drying ratio is', drying_ratio
+            write(*,*) 'NI is', NI
+            write(*,*) 'miner total is', SF_val_miner_total
+            write(*,*) 'part dens is', SF_val_part_dens
+
             if(FI(p,f) > SF_val_fire_threshold .and. FI(p,f) > FI_init(p))then
                 call CrownFireBehaveFM10(drying_ratio, NI, &
                 SF_val_miner_total, SF_val_part_dens, wind, &
@@ -266,11 +273,13 @@ program FatesTestCanopyFuel
                 fuel(f)%non_trunk_loading, fuel(f)%average_moisture_notrunks,   &
                 fuel(f)%MEF_notrunks, NI, effect_wind, ROS, i_r)
 
-                write (*,*) 'ROS_SA now is: ', ROS
-                write (*,*) 'ROS_front now is: ', ROS_front(p,f)
-
                 HPA = HeatReleasePerArea(fuel(f)%SAV_notrunks, i_r)
                 ROS_init = (60.0_r8 * FI_init(p)) / HPA
+
+                write (*,*) 'ROS_SA now is: ', ROS
+                write (*,*) 'ROS_front now is: ', ROS_front(p,f)
+                write(*,*) 'ROS_init is:', ROS_init
+                
                 ! calculate crown fraction burnt
                 call CrownFireCFB(ROS_active, ROS_critical(p), ROS_front(p,f), &
                 ROS_init, ROS, active_crownfire, passive_crownfire, crown_frac_burnt)
