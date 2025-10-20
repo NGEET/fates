90f9485dd6575a942ef9b47b9c252ba019613810
Xiulin Gao <xiulin.gao.edu@gmail.com>
2025-10-19 21:02:49 -0700

    args pass

--- testing/functional_testing/fire/fuel/FatesTestFuel.F90
+++ testing/functional_testing/fire/fuel/FatesTestFuel.F90
@@ -97,9 +97,9 @@ program FatesTestFuel
       call SetUpFuel(fuel(f), fuel_models_array, fuel_models(f), fuel_names(f), carriers(f))
 
       ! sum up fuel and calculate loading
-      call fuel%CalculateWeightingFactor(SF_val_SAV, SF_val_part_dens)
-      call fuel(f)%SumLoading()
-      call fuel(f)%CalculateFractionalLoading()
+      call fuel(f)%CalculateWeightingFactor(SF_val_SAV, SF_val_part_dens)
+      call fuel(f)%SumLoading(SF_val_SAV, SF_val_part_dens)
+      call fuel(f)%CalculateFractionalLoading(SF_val_SAV, SF_val_part_dens)
 
       ! calculate geometric properties
       call fuel(f)%AverageBulkDensity(SF_val_FBD)
